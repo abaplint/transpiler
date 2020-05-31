30923dd4144829c0c844b4dddfbea3e3fdc01d22
Lars Hvam <larshp@hotmail.com>
2020-05-31 11:45:55 +0200

    0.4.11, interface handling (#30)
    * also make sure interfaces are implemented
    
    * refactor test fixture
    
    * another test, CLAS + INTF
    
    * fix interfaces
    
    * 0.4.11

--- packages/abap-loader/src/loader.ts
+++ packages/abap-loader/src/loader.ts
@@ -1,7 +1,25 @@
 import * as transpiler from "@abaplint/transpiler";
+import * as path from "path";
 
-export default async function ABAPLoader(source: any, _map: any, _meta: any) {
+async function transpile(source: string) {
+  const files: transpiler.IFile[] = [{filename: this.resourcePath, contents: source}];
+
+  const dir = path.dirname(this.resourcePath);
+  this.addContextDependency(dir);
+  for (const f of this.fs.readdirSync(dir)) {
+    const filename = dir + path.sep + f;
+    if (filename === this.resourcePath) {
+      continue;
+    }
+    files.push({filename, contents: this.fs.readFileSync(filename).toString()});
+  }
+
   const t = new transpiler.Transpiler();
-  const result = await t.run([{filename: this.resourcePath, contents: source}]);
+  return t.run(files);
+}
+
+export default async function ABAPLoader(source: string, _map: any, _meta: any) {
+  const result = await transpile.bind(this)(source);
+
   return result.js[0].contents;
 }
--- packages/abap-loader/test/loader.ts
+++ packages/abap-loader/test/loader.ts
@@ -1,14 +1,75 @@
 import {expect} from "chai";
-import compiler from "./compiler";
+import {run, IFixture} from "./compiler";
 
 describe("ABAP", () => {
 
-  it("Test 1", async () => {
-    const stats = await compiler("zprogram.prog.abap");
-    // @ts-ignore
-    const output = stats.toJson().modules[0].source;
+  it("Simple PROG", async () => {
+    const fix: IFixture = {
+      entryFilename: "zprogram.prog.abap",
+      files: [{
+        filename: "zprogram.prog.abap",
+        contents: "WRITE 'sdf'."}],
+    };
+    const stats = await run(fix);
 
+    const output = stats.toJson().modules![0].source;
     expect(output).to.equal(`abap.statements.write('sdf');`);
   });
+
+  it("Parser error", async () => {
+    const fix: IFixture = {
+      entryFilename: "zprogram.prog.abap",
+      files: [{
+        filename: "zprogram.prog.abap",
+        contents: "parser error!!!"}],
+    };
+    await run(fix)
+      .then(() => expect.fail("expected error"))
+      .catch((reason) => expect(reason.toString()).to.contain("arser"));
+  });
+
+  it("Simple CLAS", async () => {
+    const fix: IFixture = {
+      entryFilename: "zcl_foobar.clas.abap",
+      files: [{
+        filename: "zcl_foobar.clas.abap",
+        contents: `
+CLASS zcl_foobar DEFINITION PUBLIC FINAL CREATE PUBLIC.
+ENDCLASS.
+CLASS zcl_foobar IMPLEMENTATION.
+ENDCLASS.`}],
+    };
+    const stats = await run(fix);
+
+    const output = stats.toJson().modules![0].source;
+    expect(output).to.contain(`export class`);
+  });
+
+  it("CLAS + INTF", async () => {
+    const fix: IFixture = {
+      entryFilename: "zcl_foobar.clas.abap",
+      files: [{
+        filename: "zcl_foobar.clas.abap",
+        contents: `
+CLASS zcl_foobar DEFINITION PUBLIC FINAL CREATE PUBLIC.
+  PUBLIC SECTION.
+    INTERFACES zif_foobar.
+ENDCLASS.
+CLASS zcl_foobar IMPLEMENTATION.
+  METHOD zif_foobar~run.
+  ENDMETHOD.
+ENDCLASS.`}, {
+        filename: "zif_foobar.intf.abap",
+        contents: `
+INTERFACE zif_foobar PUBLIC..
+  METHODS: run.
+ENDINTERFACE.`}],
+    };
+    const stats = await run(fix);
+
+    const output = stats.toJson().modules![0].source;
+    expect(output).to.contain(`export class`);
+  });
+
 
 });
