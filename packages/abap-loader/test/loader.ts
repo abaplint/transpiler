72d6f93037ddd0e06142d7493e0df7d3625450a1
larshp <larshp@hotmail.com>
2020-05-31 09:49:58 +0200

    another test, CLAS + INTF

--- packages/abap-loader/test/loader.ts
+++ packages/abap-loader/test/loader.ts
@@ -3,7 +3,7 @@ import {run, IFixture} from "./compiler";
 
 describe("ABAP", () => {
 
-  it("Test 1", async () => {
+  it("Simple PROG", async () => {
     const fix: IFixture = {
       entryFilename: "zprogram.prog.abap",
       files: [{
@@ -27,5 +27,50 @@ describe("ABAP", () => {
       .then(() => expect.fail("expected error"))
       .catch((reason) => expect(reason.toString()).to.contain("arser"));
   });
+
+  it("Simple CLAS", async () => {
+    const fix: IFixture = {
+      entryFilename: "zcl_foobar.clas.abap",
+      files: [{
+        filename: "zcl_foobar.clas.abap",
+        contents: `
+CLASS zcl_foobar DEFINITION PUBLIC FINAL CREATE PUBLIC.
+ENDCLASS.
+CLASS zcl_foobar IMPLEMENTATION.
+ENDCLASS.`}],
+    };
+    const stats = await run(fix);
+
+    const output = stats.toJson().modules![0].source;
+    expect(output).to.contain(`export class`);
+  });
+
+  it.skip("CLAS + INTF", async () => {
+    const fix: IFixture = {
+      entryFilename: "zcl_foobar.clas.abap",
+      files: [{
+        filename: "zcl_foobar.clas.abap",
+        contents: `
+CLASS zcl_foobar DEFINITION PUBLIC FINAL CREATE PUBLIC.
+  PUBLIC SECTION.
+    INTERFACES zif_foobar.
+ENDCLASS.
+CLASS zcl_foobar IMPLEMENTATION.
+  METHOD zif_foobar~run.
+  ENDMETHOD.
+ENDCLASS.`}, {
+        filename: "zif_foobar.intf.abap",
+        contents: `
+INTERFACE zif_foobar DEFINITION PUBLIC.
+  METHODS: run.
+ENDINTERFACE.`}],
+    };
+    const stats = await run(fix);
+
+    const output = stats.toJson().modules![0].source;
+    console.dir(output);
+    expect(output).to.contain(`export class`);
+  });
+
 
 });
