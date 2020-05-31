5b020f041672b1af6bcdfa1a9b882ec767c1aefd
larshp <larshp@hotmail.com>
2020-05-31 09:34:16 +0200

    refactor test fixture

--- packages/abap-loader/test/compiler.ts
+++ packages/abap-loader/test/compiler.ts
@@ -2,11 +2,16 @@ import * as path from "path";
 import * as webpack from "webpack";
 import * as memfs from "memfs";
 
-export default function run(fixture: any, options = {}) {
+export interface IFixture {
+  entryFilename: string,
+  files: {filename: string, contents: string}[],
+}
+
+export function run(fixture: IFixture, options = {}): Promise<webpack.Stats> {
   const compiler = webpack({
     context: __dirname,
     target: "node",
-    entry: `./${fixture}`,
+    entry: `./${fixture.entryFilename}`,
     devtool: "nosources-source-map",
     output: {
       path: path.resolve(__dirname),
@@ -23,10 +28,13 @@ export default function run(fixture: any, options = {}) {
     },
   });
 
-  const input = memfs.createFsFromVolume(memfs.Volume.fromJSON({
-    "./zprogram.prog.abap": "WRITE 'sdf'.",
-    "../src/loader.js": "",  // there is something strange here, it needs to be able to resolve from virtual file system, but the file is read from physical?
-  }, __dirname));
+  // there is something strange here, it needs to be able to resolve from virtual file system, but the file is read from physical?
+  const jsonFiles: any = {"../src/loader.js": ""};
+  for (const f of fixture.files) {
+    jsonFiles[f.filename] = f.contents;
+  }
+
+  const input = memfs.createFsFromVolume(memfs.Volume.fromJSON(jsonFiles, __dirname));
   // @ts-ignore
   compiler.inputFileSystem = input;
   // @ts-ignore
--- packages/abap-loader/test/loader.ts
+++ packages/abap-loader/test/loader.ts
@@ -1,14 +1,31 @@
 import {expect} from "chai";
-import compiler from "./compiler";
+import {run, IFixture} from "./compiler";
 
 describe("ABAP", () => {
 
   it("Test 1", async () => {
-    const stats = await compiler("zprogram.prog.abap");
-    // @ts-ignore
-    const output = stats.toJson().modules[0].source;
+    const fix: IFixture = {
+      entryFilename: "zprogram.prog.abap",
+      files: [{
+        filename: "zprogram.prog.abap",
+        contents: "WRITE 'sdf'."}],
+    };
+    const stats = await run(fix);
 
+    const output = stats.toJson().modules![0].source;
     expect(output).to.equal(`abap.statements.write('sdf');`);
+  });
+
+  it("Parser error", async () => {
+    const fix: IFixture = {
+      entryFilename: "zprogram.prog.abap",
+      files: [{
+        filename: "zprogram.prog.abap",
+        contents: "parser error!!!"}],
+    };
+    await run(fix)
+      .then(() => expect.fail("expected error"))
+      .catch((reason) => expect(reason.toString()).to.contain("arser"));
   });
 
 });
