b305cb939ea96c2bb128140995c3ee744db7db46
Lars Hvam <larshp@hotmail.com>
2020-08-08 10:09:26 +0200

    update deps (#70)

--- packages/transpiler/src/statements/data.ts
+++ packages/transpiler/src/statements/data.ts
@@ -6,7 +6,7 @@ import {Traversal} from "../traversal";
 export class DataTranspiler implements IStatementTranspiler {
 
   public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): string {
-    const token = node.findFirstExpression(abaplint.Expressions.NamespaceSimpleName)!.getFirstToken();
+    const token = node.findFirstExpression(abaplint.Expressions.DefinitionName)!.getFirstToken();
 
     const scope = traversal.getSpaghetti().lookupPosition(token.getStart(), traversal.getFilename());
     if (scope === undefined) {
