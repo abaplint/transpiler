5253d9d749f922abc325da49f3c0f2579dc9ae65
larshp <larshp@hotmail.com>
2020-09-26 13:07:03 +0200

    add cli

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,32 @@
+import * as Transpiler from "@abaplint/transpiler";
+import * as fs from "fs";
+import * as path from "path";
+import * as glob from "glob";
+
+async function run() {
+  console.log("Transpiler CLI");
+
+  const files: Transpiler.IFile[] = [];
+  for (let filename of glob.sync("src/**", {nosort: true, nodir: true})) {
+    const contents = fs.readFileSync(filename, "utf8");
+    filename = path.basename(filename);
+    files.push({filename, contents});
+    console.log(filename);
+  }
+
+  const t = new Transpiler.Transpiler();
+  const output = await t.run(files);
+
+  fs.mkdirSync("output");
+  for (const o of output) {
+    console.log(o.js.filename);
+    fs.writeFileSync("output" + path.sep + o.js.filename, o.js.contents);
+  }
+}
+
+run().then(() => {
+  process.exit();
+}).catch((err) => {
+  console.log(err);
+  process.exit(1);
+});
