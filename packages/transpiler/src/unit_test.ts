4d78cdee398f55651dabe0082cbd11810d6dd5a9
larshp <larshp@hotmail.com>
2020-09-28 06:44:15 +0200

    setup and teardown

--- packages/transpiler/src/unit_test.ts
+++ packages/transpiler/src/unit_test.ts
@@ -3,7 +3,6 @@ import * as abaplint from "@abaplint/core";
 export class UnitTest {
 
   // with lots of assumptions regarding setup
-  // todo: SETUP and TEARDOWN methods
   public run(reg: abaplint.IRegistry): string {
     let ret = `global.abap = require("@abaplint/runtime");\n`;
 
@@ -15,14 +14,35 @@ export class UnitTest {
               continue;
             }
             ret += `{
-const ${def.name} = require("./output/${obj.getName().toLowerCase()}.${obj.getType().toLowerCase}").${def.name};
+const ${def.name} = require("./${obj.getName().toLowerCase()}.${obj.getType().toLowerCase()}").${def.name};
 const test = new ${def.name}();\n`;
+
+            if (def.methods.some(m => m.name.toUpperCase() === "CLASS_SETUP")) {
+              ret += `test.class_setup();\n`;
+            }
+
+            const hasSetup = def.methods.some(m => m.name.toUpperCase() === "SETUP");
+            const hasTeardown = def.methods.some(m => m.name.toUpperCase() === "TEARDOWN");
+
             for (const m of def.methods) {
               if (m.isForTesting === false) {
                 continue;
               }
+              if (hasSetup === true) {
+                ret += `test.setup();\n`;
+              }
+
               ret += `test.${m.name}();\n`;
+
+              if (hasTeardown === true) {
+                ret += `test.teardown();\n`;
+              }
             }
+
+            if (def.methods.some(m => m.name.toUpperCase() === "CLASS_TEARDOWN")) {
+              ret += `test.class_teardown();\n`;
+            }
+
             ret += `}\n`;
           }
         }
