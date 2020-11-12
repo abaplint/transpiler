3d2b77d5b2af3eb9a8cd8894463d77f6169dbf06
larshp <larshp@hotmail.com>
2020-11-12 09:49:44 +0100

    update deps

--- packages/transpiler/src/validation.ts
+++ packages/transpiler/src/validation.ts
@@ -27,7 +27,7 @@ export const config: IConfig = {
     },
     "parser_error": true,
     "allowed_object_types": {
-      "allowed": ["INTF", "CLAS", "PROG", "DEVC", "TABL", "DTEL", "DOMA"],
+      "allowed": ["INTF", "CLAS", "PROG", "DEVC", "TABL", "DTEL", "DOMA", "TTYP"],
     },
     "unknown_types": true,
     "ambiguous_statement": true,
