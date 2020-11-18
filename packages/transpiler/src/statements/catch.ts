c188a45a04e80a788cfe3773491caf55e816d80c
larshp <larshp@hotmail.com>
2020-11-18 14:46:01 +0100

    CATCH INTO

--- packages/transpiler/src/statements/catch.ts
+++ packages/transpiler/src/statements/catch.ts
@@ -1,11 +1,19 @@
 import * as abaplint from "@abaplint/core";
+import {Traversal} from "../traversal";
 import {IStatementTranspiler} from "./_statement_transpiler";
 
 export class CatchTranspiler implements IStatementTranspiler {
 
-  public transpile(_node: abaplint.Nodes.StatementNode): string {
+  public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): string {
+
+    let into = "";
+    const intoNode = node.findExpressionAfterToken("INTO");
+    if (intoNode) {
+      into = "\n" + traversal.traverse(intoNode) + ".set(e);";
+    }
+
     // todo, add more here
-    return "} catch (e) {";
+    return "} catch (e) {" + into;
   }
 
 }
