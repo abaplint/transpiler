d38bb29c19972144edbce179a5615fe26ba8dd63
larshp <larshp@hotmail.com>
2020-11-27 15:36:32 +0100

    fix web

--- web/transpiler/webpack.config.js
+++ web/transpiler/webpack.config.js
@@ -19,6 +19,8 @@ module.exports = {
     fallback: {
       "buffer": require.resolve("buffer/"),
       "stream": require.resolve("stream-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "path": require.resolve("path-browserify"),
       "string_decoder": require.resolve("string_decoder/"),
     },
     extensions: [".ts", ".js"],
