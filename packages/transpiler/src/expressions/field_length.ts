fd870c7c4806a3673a90e721ee379a6e6e19e710
joltdx <jorgen.lindqvist@joltdx.se>
2020-12-11 08:50:09 +0100

    reworked field offsets and lengths

--- packages/transpiler/src/expressions/field_length.ts
+++ packages/transpiler/src/expressions/field_length.ts
@@ -1,15 +1,33 @@
-import {Nodes} from "@abaplint/core";
+import {Expressions, Nodes, Tokens} from "@abaplint/core";
 import {IExpressionTranspiler} from "./_expression_transpiler";
 
 export class FieldLengthTranspiler implements IExpressionTranspiler {
 
   public transpile(node: Nodes.ExpressionNode): string {
-    const children = node.getChildren();
-    const str = children[1].getFirstToken().getStr();
-    if (/^\d+$/.test(str)) {
-      return str;
+    let ret = "";
+    for (const c of node.getChildren()) {
+      if (c instanceof Nodes.ExpressionNode) {
+        if (c.get() instanceof Expressions.SourceField) {
+          let sourceStr = c.getFirstToken().getStr();
+          if (sourceStr === "sy") {
+            sourceStr = "abap.builtin.sy";
+          }
+          ret += sourceStr;
+        } else if (c.get() instanceof Expressions.ArrowOrDash) {
+          ret += ".get().";
+        } else if (c.get() instanceof Expressions.ComponentName) {
+          ret += c.getFirstToken().getStr();
+        }
+      } else if(c instanceof Nodes.TokenNode) {
+        if (c.get() instanceof Tokens.Identifier) {
+          ret += c.getFirstToken().getStr();
+        }
+      }
+    }
+    if (/^\d+$/.test(ret)) {
+      return ret;
     } else {
-      return str + ".get()";
+      return ret + ".get()";
     }
   }
 
