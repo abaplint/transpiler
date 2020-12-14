b52d84cfac9a43852fd670629784e776158e3551
larshp <larshp@hotmail.com>
2020-12-14 14:26:47 +0100

    generic sort

--- packages/runtime/src/statements/sort.ts
+++ packages/runtime/src/statements/sort.ts
@@ -3,29 +3,43 @@ import {eq, lt, gt} from "../compare";
 
 export interface ISortOptions {
   descending?: boolean,
-  by?: {component: string, descending: boolean}[],
+  by?: {component: string, descending?: boolean}[],
+}
+
+function compare(a: any, b: any, input: {component: string, descending?: boolean}) {
+  const componentName = input.component;
+  const descending = input.descending;
+
+  const vala = a.get()[componentName];
+  const valb = b.get()[componentName];
+
+  if (eq(vala,valb)) {
+    return 0;
+  } else if (descending && gt(vala, valb))  {
+    return -1;
+  } else if (!descending && lt(vala, valb)) {
+    return -1;
+  } else {
+    return 1;
+  }
 }
 
 export function sort(input: Table, options?: ISortOptions) {
   const items = input.array();
 
   if (options?.by) {
-    const componentName = options.by[0].component;
-    const descending = options.by[0].descending;
+    if (options.by.length === 0) {
+      throw "SortByLengthZero";
+    }
 
     items.sort((a, b) => {
-      const vala = a.get()[componentName];
-      const valb = b.get()[componentName];
-
-      if (eq(vala,valb)) {
-        return 0;
-      } else if (descending && gt(vala, valb))  {
-        return -1;
-      } else if (!descending && lt(vala, valb)) {
-        return -1;
-      } else {
-        return 1;
+      for (const c of options.by || []) {
+        const res = compare(a, b, c);
+        if (res !== 0) {
+          return res;
+        }
       }
+      return 0;
     });
 
   } else {
