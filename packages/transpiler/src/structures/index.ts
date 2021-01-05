334759a6a524d3e9d51911adeca76c88f07271af
larshp <larshp@hotmail.com>
2021-01-05 10:05:34 +0100

    DATA begin, closes #273

--- packages/transpiler/src/structures/index.ts
+++ packages/transpiler/src/structures/index.ts
@@ -2,6 +2,7 @@ export * from "./case";
 export * from "./class_definition";
 export * from "./class_implementation";
 export * from "./constants";
+export * from "./data";
 export * from "./function_module";
 export * from "./interface";
 export * from "./types";
---
+++ test/statements/data.ts
@@ -0,0 +1,31 @@
+// import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - DATA", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("DATA, with BEGIN OF", async () => {
+    const code = `
+DATA: BEGIN OF ls_msg,
+        a1 TYPE c LENGTH 1,
+        a2 TYPE c LENGTH 1,
+        a3 TYPE c LENGTH 1,
+        a4 TYPE c LENGTH 1,
+      END OF ls_msg.
+ls_msg-a3 = 'A'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
