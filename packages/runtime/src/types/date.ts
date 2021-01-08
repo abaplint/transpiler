f6066725d031376cc8ce2ce0e4fe29e94f823330
joltdx <jorgen.lindqvist@joltdx.se>
2021-01-08 13:50:45 +0100

    More with the date and time

--- packages/runtime/src/types/date.ts
+++ packages/runtime/src/types/date.ts
@@ -1,6 +1,7 @@
 import {Hex} from "./hex";
 import {ICharacter} from "./_character";
 import {INumeric} from "./_numeric";
+import {getDateFromNumber, getNumberFromDate} from "./_javascript_date";
 
 export class Date implements ICharacter {
   private value: string;
@@ -11,7 +12,11 @@ export class Date implements ICharacter {
 
   public set(value: INumeric | ICharacter | Hex | string | number) {
     if (typeof value === "number") {
-      this.value = value.toString().padStart(8,"0");
+      if (value <= 0 || value > 3652060) {
+        this.value = "00000000";
+      } else {
+        this.value = getDateFromNumber(value);
+      }
     } else if (typeof value === "string") {
       this.value = value;
     } else {
@@ -26,5 +31,9 @@ export class Date implements ICharacter {
 
   public get(): string {
     return this.value;
+  }
+
+  public getNumeric(): number {
+    return getNumberFromDate(this.value);
   }
 }
--- packages/runtime/src/types/time.ts
+++ packages/runtime/src/types/time.ts
@@ -11,7 +11,11 @@ export class Time implements ICharacter {
 
   public set(value: INumeric | ICharacter | Hex | string | number) {
     if (typeof value === "number") {
-      this.value = value.toString().padStart(6,"0");
+      const date = new Date();
+      date.setTime(value * 1000);
+      this.value = date.getUTCHours().toString().padStart(2,"0") +
+                   date.getUTCMinutes().toString().padStart(2,"0") +
+                   date.getUTCSeconds().toString().padStart(2,"0");
     } else if (typeof value === "string") {
       this.value = value;
     } else {
@@ -26,5 +30,12 @@ export class Time implements ICharacter {
 
   public get(): string {
     return this.value;
+  }
+
+  public getNumeric(): number {
+    const hours = parseInt(this.value.substr(0,2),10);
+    const minutes = parseInt(this.value.substr(2,2),10);
+    const seconds = parseInt(this.value.substr(4,2),10);
+    return hours * 3600 + minutes * 60 + seconds;
   }
 }
