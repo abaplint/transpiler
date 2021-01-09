6b8b59524b52dd230e9403114969788151efbfb0
larshp <larshp@hotmail.com>
2021-01-09 16:29:12 +0100

    structured set

--- packages/runtime/src/types/structure.ts
+++ packages/runtime/src/types/structure.ts
@@ -1,4 +1,5 @@
 import {clone} from "../clone";
+import {ICharacter} from "./_character";
 
 export class Structure {
   private readonly value: any;
@@ -15,18 +16,37 @@ export class Structure {
     return this;
   }
 
-  public set(input: Structure | undefined) {
-    // todo, validate input stucture
+  public set(input: Structure | string | ICharacter | undefined) {
     if (input === undefined) {
       return;
     }
 
-    const obj = input.get();
-    for (const f in obj) {
-      // @ts-ignore
-      this.value[f].set(clone(obj[f]));
+    if (input instanceof Structure) {
+      const obj = input.get();
+      for (const f in obj) {
+        // @ts-ignore
+        this.value[f].set(clone(obj[f]));
+      }
+    } else {
+      this.setCharacter(input);
     }
+
     return this;
+  }
+
+  private setCharacter(input: string | ICharacter) {
+    this.clear();
+
+    let val = input;
+    if (typeof val !== "string") {
+      val = val.get();
+    }
+
+    for (const key of Object.keys(this.value)) {
+      const targetLength = this.value[key].getLength();
+      this.value[key].set(val.substr(0, targetLength));
+      val = val.substr(targetLength);
+    }
   }
 
   public get() {
