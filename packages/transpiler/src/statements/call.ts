44d8f279c6229fd4c8d454cb4442bbbf864632ea
larshp <larshp@hotmail.com>
2021-01-09 17:29:57 +0100

    bugfix, CALL METHOD super->constructor

--- packages/transpiler/src/statements/call.ts
+++ packages/transpiler/src/statements/call.ts
@@ -5,7 +5,7 @@ import {Traversal} from "../traversal";
 export class CallTranspiler implements IStatementTranspiler {
 
   public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): string {
-    if (node.concatTokens().toLowerCase().startsWith("super->constructor(")) {
+    if (node.concatTokens().toLowerCase().includes("super->constructor(")) {
       // todo, https://github.com/abaplint/transpiler/issues/133
       return "";
     }
