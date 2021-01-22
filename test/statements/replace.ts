c6baef7b515aa69d9e15a78dd4a06446107d13f9
larshp <larshp@hotmail.com>
2021-01-22 08:40:10 +0100

    TRANSLATE USING, closes #304

---
+++ test/statements/replace.ts
@@ -0,0 +1,29 @@
+// import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - REPLACE", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("REPLACE ALL", async () => {
+    const code = `
+  DATA str TYPE string.
+  str = 'aabbccbb'.
+  REPLACE ALL OCCURRENCES OF |bb| IN str WITH |dd|.
+  ASSERT str = 'aaddccdd'.`;
+
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
