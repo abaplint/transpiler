5816f35e1f915588ca8de121fcb3ab0aeaf7b34e
larshp <larshp@hotmail.com>
2021-01-23 11:36:11 +0100

    database scaffolding, fail at runtime

---
+++ packages/transpiler/src/statements/delete_database.ts
@@ -0,0 +1,11 @@
+import * as abaplint from "@abaplint/core";
+import {IStatementTranspiler} from "./_statement_transpiler";
+import {Traversal} from "../traversal";
+
+export class DeleteDatabaseTranspiler implements IStatementTranspiler {
+
+  public transpile(_node: abaplint.Nodes.StatementNode, _traversal: Traversal): string {
+    return `throw new Error("DeleteDatabase, transpiler todo");`;
+  }
+
+}
