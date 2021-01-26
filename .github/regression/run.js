55fcd699f18493cead8644d7cf4bae9130b8631d
larshp <larshp@hotmail.com>
2021-01-26 09:54:24 +0100

    upd

--- .github/regression/run.js
+++ .github/regression/run.js
@@ -1,9 +1,28 @@
 'use strict';
 const fs = require("fs");
 
-console.dir(JSON.parse(fs.readFileSync("./results_after.json", "utf-8")));
+const after = JSON.parse(fs.readFileSync("./results_after.json", "utf-8"));
+const before = JSON.parse(fs.readFileSync("./results_before.json", "utf-8"));
 
-let comment = "hello world\n";
+const result = {};
+
+for (const a of after) {
+  if (result[a.name] === undefined) {
+    result[a.name] = {};
+  }
+  result[a.name].after = a.runtime;
+}
+
+for (const b of before) {
+  if (result[b.name] === undefined) {
+    result[b.name] = {};
+  }
+  result[b.name].before = b.runtime;
+}
+
+console.dir(results);
+
+let comment = "Regression test results: hello world\n";
 
 console.dir(comment);
 
