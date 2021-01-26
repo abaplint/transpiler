9a24be3cadf74b5165b4ff448afd1d2e4cab51ce
larshp <larshp@hotmail.com>
2021-01-26 09:34:10 +0100

    upd

---
+++ .github/regression/run.js
@@ -0,0 +1,10 @@
+'use strict';
+const fs = require("fs");
+
+console.dir(JSON.parse(fs.readFileSync("../../results_after.json", "utf-8")));
+
+let comment = "hello world";
+
+console.dir(comment);
+
+fs.writeFileSync("comment-body.txt", comment);
