d6f85359e75dfcd3b2ddcdc755282d71023af9da
Jörgen Lindqvist <jorgen.lindqvist@joltdx.se>
2021-02-13 11:41:03 +0100

    nmin for integers (#333)
    * nmin for integers
    
    * nmax for integers

--- packages/runtime/src/builtin/index.ts
+++ packages/runtime/src/builtin/index.ts
@@ -22,6 +22,8 @@ export * from "./floor";
 export * from "./frac";
 export * from "./sign";
 export * from "./trunc";
+export * from "./nmin";
+export * from "./nmax";
 export const abap_true = new Character({length: 1}).set("X");
 export const abap_false = new Character({length: 1}).set("");
 export const abap_undefined = new Character({length: 1}).set("-");
