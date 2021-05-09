ad2204e1e92690d7a8869ffa6673c908ab183b04
larshp <larshp@hotmail.com>
2021-05-09 09:40:44 +0200

    sort imports

--- packages/transpiler/src/unit_test.ts
+++ packages/transpiler/src/unit_test.ts
@@ -102,19 +102,31 @@ run().then(() => {
   private buildImports(reg: abaplint.IRegistry): string {
 // note: es modules are hoised, so use the dynamic import()
 // todo, some sorting might be required? eg. a class constructor using constant from interface?
-    let ret = "";
+
+// temporary sorting:
+// 1: dependency interfaces
+// 2: dependency classes
+// 3: interfaces
+// 4: classes
+// 5: function groups
+
+    const list: {sort: number, code: string}[] = [];
+
     for (const obj of reg.getObjects()) {
       if (obj instanceof abaplint.Objects.FunctionGroup) {
         for (const m of obj.getModules()) {
-          ret += `await import("./${obj.getName().toLowerCase()}.fugr.${m.getName().toLowerCase()}.mjs");\n`;
+          list.push({sort: 5, code: `await import("./${obj.getName().toLowerCase()}.fugr.${m.getName().toLowerCase()}.mjs");`});
         }
       } else if (obj instanceof abaplint.Objects.Class) {
-        ret += `await import("./${obj.getName().toLowerCase()}.clas.mjs");\n`;
+        list.push({sort: 5, code: `await import("./${obj.getName().toLowerCase()}.clas.mjs");`});
       } else if (obj instanceof abaplint.Objects.Interface) {
-        ret += `await import("./${obj.getName().toLowerCase()}.intf.mjs");\n`;
+        list.push({sort: 5, code: `await import("./${obj.getName().toLowerCase()}.intf.mjs");`});
       }
     }
-    return ret;
+
+    list.sort((a, b) => { return a.sort - b.sort; });
+
+    return list.map(a => a.code).join("\n") + "\n";
   }
 
 }
