ef1be17b3e252e8dd8cf8da990caf19559f116d0
arcanist123 <dmitry.yudin.a@gmail.com>
2021-05-27 00:26:56 +0200

    write on structures

--- packages/runtime/src/statements/write.ts
+++ packages/runtime/src/statements/write.ts
@@ -1,3 +1,4 @@
+import {Structure} from "../types";
 import {FieldSymbol} from "../types/field_symbol";
 import {ICharacter} from "../types/_character";
 import {INumeric} from "../types/_numeric";
@@ -12,7 +13,15 @@ export function write(source: INumeric | ICharacter | FieldSymbol | string | num
     if (typeof source === "string" || typeof source === "number") {
       this.console.add(source.toString());
     } else {
-      this.console.add(source.get().toString());
+      if (source instanceof Structure) {
+        const obj = source.get();
+        for (const f in obj) {
+          // @ts-ignore
+          abap.statements.write(obj[f]);
+        }
+      } else {
+        this.console.add(source.get().toString());
+      }
     }
   }
 
