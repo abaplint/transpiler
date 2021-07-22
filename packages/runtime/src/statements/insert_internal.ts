90000f3ee1765bdc4a2c958c4b3694f6c405b830
Lars Hvam <larshp@hotmail.com>
2021-07-22 18:05:59 +0200

    INSERT INTERNAL

--- packages/runtime/src/statements/insert_internal.ts
+++ packages/runtime/src/statements/insert_internal.ts
@@ -24,6 +24,15 @@ export function insertInternal(
     for (const i of options.data.array()) {
       options.table.append(i);
     }
+  } else if (options.initial === true) {
+    let index = options.table.array().length;
+    if (options.index) {
+      index = options.index.get() - 1;
+    }
+    const val = options.table.insertIndex(options.table.getRowType(), index);
+    if (options.assigning) {
+      options.assigning.assign(val);
+    }
   } else if (options.data) {
 // todo, for now it just appends, this is not correct, but currently the table type is not known
     const val = options.table.insertIndex(options.data, options.table.array().length);
