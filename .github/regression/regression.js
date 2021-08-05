383ff0c7c6488484351ba0f30b748b34ac65a861
larshp <larshp@hotmail.com>
2021-08-05 09:42:41 +0200

    wip

--- .github/regression/regression.js
+++ .github/regression/regression.js
@@ -9,7 +9,7 @@ const repos = [
 //  {name: "larshp/abap-wasm",                 command: "npm test"},
   {name: "open-abap/open-abap",              command: "npm test"},
 //  {name: "abap-openapi/abap-openapi-client", command: "npm test"},
-//  {name: "larshp/abapNTLM",                  command: "npm test"},
+  {name: "larshp/abapNTLM",                  command: "npm test"},
 ];
 
 const CWD = "./.github/regression/";
