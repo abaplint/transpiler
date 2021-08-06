140ea83773bfa3ecfa6b08c074ebba2cf8d5056d
larshp <larshp@hotmail.com>
2021-08-06 13:25:58 +0200

    concatenate

--- packages/transpiler/src/statements/concatenate.ts
+++ packages/transpiler/src/statements/concatenate.ts
@@ -8,23 +8,28 @@ export class ConcatenateTranspiler implements IStatementTranspiler {
   public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): Chunk {
     const concat = node.concatTokens();
 
-    const slist = [];
+    const slist: Chunk[] = [];
     for (const s of node.findDirectExpressions(abaplint.Expressions.Source)) {
-      slist.push(traversal.traverse(s).getCode());
+      slist.push(traversal.traverse(s));
     }
 
     let extra = "";
     if (node.findExpressionAfterToken("BY")) {
-      extra = `, separatedBy: ${slist.pop()}`;
+      extra = `, separatedBy: ${slist.pop()?.getCode()}`;
     }
 
     if (concat.startsWith("CONCATENATE LINES OF ")) {
       extra += ", lines: true";
     }
 
-    const target = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Target)).getCode();
+    const target = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Target));
 
-    return new Chunk("abap.statements.concatenate({source: [" + slist.join(",") + "], target: " + target + extra + "});");
+    return new Chunk()
+      .append("abap.statements.concatenate({source: [", node, traversal)
+      .join(slist)
+      .appendString("], target: ")
+      .appendChunk(target)
+      .append(extra + "});", node.getLastToken(), traversal);
   }
 
 }
