47ac9284eb98d04e6aa52698d32976add7d2f836
larshp <larshp@hotmail.com>
2021-08-06 09:18:54 +0200

    add testcase

--- packages/transpiler/test/source_map.ts
+++ packages/transpiler/test/source_map.ts
@@ -1,5 +1,22 @@
 import {expect} from "chai";
+import * as sourceMap from "source-map";
 import {runSingleMapped} from "./_utils";
+
+async function countMappingsPerGeneratedLine(map: string | undefined) {
+  if (map === undefined) {
+    return {};
+  }
+  const ret: { [index: number]: number } = {};
+  const consumer = await new sourceMap.SourceMapConsumer(JSON.parse(map));
+  consumer.computeColumnSpans();
+  consumer.eachMapping((m) => {
+    if (ret[m.generatedLine] === undefined) {
+      ret[m.generatedLine] = 0;
+    }
+    ret[m.generatedLine]++;
+  });
+  return ret;
+}
 
 describe("Source Map", () => {
 
@@ -26,6 +43,22 @@ ENDTRY.`;
     const result = await runSingleMapped(abap, {ignoreSyntaxCheck: true});
     expect(result?.js).to.equal(js);
     expect(result?.map).to.equal(map);
+  });
+
+  it("IF", async () => {
+    const abap = `IF foo CP bar.
+ENDIF.`;
+
+    const js =
+`if (abap.compare.cp(foo, bar)) {
+}`;
+
+    const result = await runSingleMapped(abap, {ignoreSyntaxCheck: true});
+    expect(result?.js).to.equal(js);
+
+    const perLine = await countMappingsPerGeneratedLine(result?.map);
+    expect(perLine[1]).to.equal(3);
+    expect(perLine[2]).to.equal(1);
   });
 
 });
