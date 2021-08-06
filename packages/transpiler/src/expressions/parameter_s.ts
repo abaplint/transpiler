a05e7d6fb6bb01716c47cfe9d1f87d0d2079eca7
larshp <larshp@hotmail.com>
2021-08-06 08:03:47 +0200

    parameter lists

--- packages/transpiler/src/expressions/parameter_s.ts
+++ packages/transpiler/src/expressions/parameter_s.ts
@@ -6,10 +6,11 @@ import {Chunk} from "../chunk";
 export class ParameterSTranspiler implements IExpressionTranspiler {
 
   public transpile(node: Nodes.ExpressionNode, traversal: Traversal): Chunk {
-    const name = node.findDirectExpression(Expressions.ParameterName)?.getFirstToken().getStr();
-    const source = traversal.traverse(node.findDirectExpression(Expressions.Source)).getCode();
+    const nameToken = node.findDirectExpression(Expressions.ParameterName)?.getFirstToken();
+    const name = nameToken?.getStr();
+    const source = traversal.traverse(node.findDirectExpression(Expressions.Source));
 
-    return new Chunk(name + ": " + source);
+    return new Chunk().append(name + ": ", nameToken || node, traversal).appendChunk(source);
   }
 
 }
