fae88a7bcfa672e2687cac12dc80a9263404c32c
larshp <larshp@hotmail.com>
2021-09-05 13:06:17 +0200

    less strict keywords

--- packages/transpiler/src/keywords.ts
+++ packages/transpiler/src/keywords.ts
@@ -34,15 +34,15 @@ export class Keywords {
   private traverse(node: abaplint.INode, file: abaplint.ABAPFile): abaplint.Token[] {
 // https://www.w3schools.com/js/js_reserved.asp
     const keywords: string[] = [
-      "abstract",	"arguments", "await", "boolean",
+      "abstract",	"arguments", "await",
       "break",	"byte", "catch",
       "char",	"class", "const", "continue",
       "debugger",	"default", "do",
       "double",	"else", "enum", "eval",
       "export",	"extends", "false", "final",
-      "finally",	"float", "for", "function",
+      "finally", "for", "function",
       "goto",	"if", "implements", "import",
-      "in",	"instanceof", "int", "interface",
+      "in",	"instanceof", "interface",
       "let",	"long", "native", "new",
       "null",	"package", "private", "protected",
       "public",	"return", "short", "static",
--- packages/transpiler/src/validation.ts
+++ packages/transpiler/src/validation.ts
@@ -49,15 +49,15 @@ export const config: IConfig = {
     },
     "forbidden_identifier": {
       "check": [
-        "^abstract$",	"^arguments$", "^await$", "^boolean$",
+        "^abstract$",	"^arguments$", "^await$",
         "^break$",	"^byte$", "^catch$",
         "^char$",	"^class$", "^const$", "^continue$",
         "^debugger$",	"^default$", "^do$",
         "^double$",	"^else$", "^enum$", "^eval$",
         "^export$",	"^extends$", "^false$", "^final$",
-        "^finally$",	"^float$", "^for$", "^function$",
+        "^finally$", "^for$", "^function$",
         "^goto$",	"^if$", "^implements$", "^import$",
-        "^in$",	"^instanceof$", "^int$", "^interface$",
+        "^in$",	"^instanceof$", "^interface$",
         "^let$",	"^long$", "^native$", "^new$",
         "^null$",	"^package$", "^private$", "^protected$",
         "^public$",	"^return$", "^short$", "^static$",
