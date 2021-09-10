b793ffb97a0cd2554e2ef3db7130a6b4c74f168e
larshp <larshp@hotmail.com>
2021-09-10 14:36:24 +0200

    reduce nesting

--- packages/transpiler/src/unit_test.ts
+++ packages/transpiler/src/unit_test.ts
@@ -26,64 +26,62 @@ async function run() {
 try {\n`;
 
     for (const obj of reg.getObjects()) {
-      if (reg.isDependency(obj)) {
+      if (reg.isDependency(obj) || !(obj instanceof abaplint.Objects.Class)) {
         continue;
       }
-      if (obj instanceof abaplint.Objects.Class) {
-        ret += `// --------------------------------------------\n`;
-        ret += `clas = unit.addObject("${obj.getName()}");\n`;
-        for (const file of obj.getABAPFiles()) {
-          for (const def of file.getInfo().listClassDefinitions()) {
-            if (def.isForTesting === false
-                || def.isGlobal === true // todo, fix, there might be global test methods
-                || def.methods.length === 0) {
-              continue;
-            }
-            ret += `{
+      ret += `// --------------------------------------------\n`;
+      ret += `clas = unit.addObject("${obj.getName()}");\n`;
+      for (const file of obj.getABAPFiles()) {
+        for (const def of file.getInfo().listClassDefinitions()) {
+          if (def.isForTesting === false
+              || def.isGlobal === true // todo, fix, there might be global test methods
+              || def.methods.length === 0) {
+            continue;
+          }
+          ret += `{
 const {${def.name}} = await import("./${obj.getName().toLowerCase()}.${obj.getType().toLowerCase()}.testclasses.mjs");
 locl = clas.addTestClass("${def.name}");\n`;
 
-            if (def.methods.some(m => m.name.toUpperCase() === "CLASS_SETUP")) {
-              ret += `await ${def.name}.class_setup();\n`;
+          if (def.methods.some(m => m.name.toUpperCase() === "CLASS_SETUP")) {
+            ret += `await ${def.name}.class_setup();\n`;
+          }
+
+          const hasSetup = def.methods.some(m => m.name.toUpperCase() === "SETUP");
+          const hasTeardown = def.methods.some(m => m.name.toUpperCase() === "TEARDOWN");
+
+          for (const m of def.methods) {
+            if (m.isForTesting === false) {
+              continue;
             }
-
-            const hasSetup = def.methods.some(m => m.name.toUpperCase() === "SETUP");
-            const hasTeardown = def.methods.some(m => m.name.toUpperCase() === "TEARDOWN");
-
-            for (const m of def.methods) {
-              if (m.isForTesting === false) {
-                continue;
-              }
-              const skipThis = (skip || []).some(a => a.object === obj.getName() && a.class === def.name && a.method === m.name);
-              if (skipThis) {
-                ret += `  console.log('${obj.getName()}: running ${def.name}->${m.name}, skipped');\n`;
-                ret += `  meth = locl.addMethod("${m.name}");\n`;
-                ret += `  meth.skip();\n`;
-                continue;
-              }
-
-              ret += `{\n  const test = await (new ${def.name}()).constructor_();\n`;
-              if (hasSetup === true) {
-                ret += `  await test.setup();\n`;
-              }
-
-              ret += `  console.log('${obj.getName()}: running ${def.name}->${m.name}');\n`;
+            const skipThis = (skip || []).some(a => a.object === obj.getName() && a.class === def.name && a.method === m.name);
+            if (skipThis) {
+              ret += `  console.log('${obj.getName()}: running ${def.name}->${m.name}, skipped');\n`;
               ret += `  meth = locl.addMethod("${m.name}");\n`;
-              ret += `  await test.${m.name}();\n`;
-              ret += `  meth.pass();\n`;
-
-              if (hasTeardown === true) {
-                ret += `  await test.teardown();\n`;
-              }
-              ret += `}\n`;
+              ret += `  meth.skip();\n`;
+              continue;
             }
 
-            if (def.methods.some(m => m.name.toUpperCase() === "CLASS_TEARDOWN")) {
-              ret += `await ${def.name}.class_teardown();\n`;
+            ret += `{\n  const test = await (new ${def.name}()).constructor_();\n`;
+            if (hasSetup === true) {
+              ret += `  await test.setup();\n`;
             }
 
+            ret += `  console.log('${obj.getName()}: running ${def.name}->${m.name}');\n`;
+            ret += `  meth = locl.addMethod("${m.name}");\n`;
+            ret += `  await test.${m.name}();\n`;
+            ret += `  meth.pass();\n`;
+
+            if (hasTeardown === true) {
+              ret += `  await test.teardown();\n`;
+            }
             ret += `}\n`;
           }
+
+          if (def.methods.some(m => m.name.toUpperCase() === "CLASS_TEARDOWN")) {
+            ret += `await ${def.name}.class_teardown();\n`;
+          }
+
+          ret += `}\n`;
         }
       }
     }
