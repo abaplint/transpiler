2cd9cb09532065b69f08b9e43d855e6946c04746
Lars Hvam <larshp@hotmail.com>
2021-10-09 09:54:18 +0200

    basic trig

---
+++ packages/runtime/src/builtin/sin.ts
@@ -0,0 +1,15 @@
+/* eslint-disable radix */
+import {ICharacter} from "../types/_character";
+import {INumeric} from "../types/_numeric";
+
+export function sin(input: {val: number | string | ICharacter | INumeric}) {
+  let num_in: number | undefined = undefined;
+  if (typeof input.val === "number") {
+    num_in = input.val;
+  } else if (typeof input.val === "string") {
+    num_in = parseFloat(input.val);
+  } else {
+    num_in = parseFloat(input.val.get().toString());
+  }
+  return Math.sin(num_in);
+}
---
+++ test/builtin/cos.ts
@@ -0,0 +1,24 @@
+// import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Builtin functions - cos", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("cos()", async () => {
+    const code = `ASSERT cos( 0 ) = 1.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
