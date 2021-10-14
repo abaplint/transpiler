270647525443b92ad3abf6838a1e5472c4009f1b
Lars Hvam <larshp@hotmail.com>
2021-10-14 07:38:58 +0200

    basic UNASSIGN, #539

---
+++ test/statements/unassign.ts
@@ -0,0 +1,26 @@
+// import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - UNASSIGN", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("basic", async () => {
+    const code = `
+      FIELD-SYMBOLS <bar> TYPE i.
+      UNASSIGN <bar>.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
