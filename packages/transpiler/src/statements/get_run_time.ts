9f3f3725f6b7e7b3205e401887919b45ba1cabb0
Lars Hvam <larshp@hotmail.com>
2021-10-19 06:23:44 +0200

    add GET RUN TIME

---
+++ packages/transpiler/src/statements/get_run_time.ts
@@ -0,0 +1,18 @@
+import * as abaplint from "@abaplint/core";
+import {IStatementTranspiler} from "./_statement_transpiler";
+import {Traversal} from "../traversal";
+import {Chunk} from "../chunk";
+
+export class GetRunTimeTranspiler implements IStatementTranspiler {
+
+  public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): Chunk {
+    const target = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Target));
+
+    const ret = new Chunk();
+    ret.append("abap.statements.getRunTime(", node, traversal);
+    ret.appendChunk(target);
+    ret.append(");", node.getLastToken(), traversal);
+    return ret;
+  }
+
+}
