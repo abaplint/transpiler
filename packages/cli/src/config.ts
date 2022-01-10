1695293410d043619a77831924f81d56b0d08f57
Lars Hvam <larshp@hotmail.com>
2022-01-10 18:02:28 +0100

    multi libs

--- packages/cli/src/config.ts
+++ packages/cli/src/config.ts
@@ -5,9 +5,13 @@ import * as fs from "fs";
 export interface ITranspilerConfig {
   input_folder: string;
   /** list of regex, case insensitive, empty gives all files, positive list */
-  input_filter: string[];
+  input_filter?: string[];
   output_folder: string;
-  lib: string;
+  /** to be deprecated */
+  lib?: string;
+  libs?: {
+    url: string,
+  }[],
   write_unit_tests: boolean;
   write_source_map: boolean;
   options: ITranspilerOptions;
