36c657f278ec815a290bb7899ecafc3bc244c120
Lars Hvam <larshp@hotmail.com>
2022-01-11 10:45:46 +0100

    1.7.15, add decfloat34 (#593)
    * add decfloat34
    
    * 1.7.15

---
+++ test/types/decfloat34.ts
@@ -0,0 +1,31 @@
+import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src/";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zdecfloat34.prog.abap", contents}]);
+}
+
+describe("Running Examples - Decfloat34 type", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("basic", async () => {
+    const code = `
+DATA foo TYPE decfloat34.
+foo = 1.
+WRITE / foo.
+
+foo = '1.12345'.
+WRITE / foo.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("1\n1,12345");
+  });
+
+});
