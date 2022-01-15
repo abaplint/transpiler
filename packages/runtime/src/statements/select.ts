41689583bcaf8f3a523ad13b933aa311ba9a43bc
Lars Hvam <larshp@hotmail.com>
2022-01-15 09:13:31 +0100

    throw cx_sy_dynamic_osql_semantics in SELECT

--- packages/runtime/src/statements/select.ts
+++ packages/runtime/src/statements/select.ts
@@ -2,7 +2,22 @@ import {clone} from "../clone";
 import {Structure, Table} from "../types";
 
 export function select(target: Structure | Table, select: string) {
-  const res = this.db.exec(select);
+  if (this.db === undefined) {
+    throw new Error("Runtime, database not initialized");
+  }
+
+  let res: undefined | any = undefined;
+  try {
+    res = this.db.exec(select);
+  } catch (error) {
+    // @ts-ignore
+    if (abap.Classes["CX_SY_DYNAMIC_OSQL_SEMANTICS"] !== undefined) {
+      // @ts-ignore
+      throw new abap.Classes["CX_SY_DYNAMIC_OSQL_SEMANTICS"]();
+    }
+    throw error;
+  }
+
   target.clear();
   if (target instanceof Structure && res[0]?.values[0]) {
     const columns: string[] = res[0].columns;
