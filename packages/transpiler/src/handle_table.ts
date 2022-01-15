d4414fa04b88c92f552a9600b7c0e518e50d44c8
Lars Hvam <larshp@hotmail.com>
2022-01-15 15:50:31 +0100

    output type

--- packages/transpiler/src/handle_table.ts
+++ packages/transpiler/src/handle_table.ts
@@ -1,18 +1,21 @@
 import * as abaplint from "@abaplint/core";
 import {Chunk} from "./chunk";
+import {TranspileTypes} from "./transpile_types";
 import {IOutputFile} from "./types";
 
 // tables or structures
 export class HandleTable {
-  public runObject(obj: abaplint.Objects.Table, _reg: abaplint.IRegistry): IOutputFile[] {
+  public runObject(obj: abaplint.Objects.Table, reg: abaplint.IRegistry): IOutputFile[] {
 
     const filename = obj.getXMLFile()?.getFilename().replace(".xml", ".mjs").toLowerCase();
     if (filename === undefined) {
       return [];
     }
 
+    const type = obj.parseType(reg);
+
     const chunk = new Chunk().appendString(`abap.DDIC["${obj.getName().toUpperCase()}"] = {
-  "type": undefined,
+  "type": ${new TranspileTypes().toType(type)},
 };`);
 
     const output: IOutputFile = {
