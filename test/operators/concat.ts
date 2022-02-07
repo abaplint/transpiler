22decd278a74b5ba358e9608291de5db0cf51c05
Lars Hvam <larshp@hotmail.com>
2022-02-07 18:42:51 +0100

    concat, bugfix

---
+++ test/operators/concat.ts
@@ -0,0 +1,31 @@
+// import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running operators - concat", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("char ends with newline", async () => {
+    const code = `
+    DATA char1 TYPE c LENGTH 3.
+    DATA char2 TYPE c LENGTH 3.
+    DATA res TYPE string.
+    char1 = |ab\\n|.
+    char2 = |cde|.
+    res = char1 && char2.
+    ASSERT res = |ab\\ncde|.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
