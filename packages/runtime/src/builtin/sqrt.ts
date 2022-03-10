78dc3c1d2e02396802a81f189e725e9cd6947eb1
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2022-03-10 07:17:30 +0100

    Add built-in sqrt (#655)
    * Create sqrt.ts
    
    * Update index.ts
    
    * Test

---
+++ packages/runtime/src/builtin/sqrt.ts
@@ -0,0 +1,18 @@
+/* eslint-disable radix */
+import {Float} from "../types";
+import {ICharacter} from "../types/_character";
+import {INumeric} from "../types/_numeric";
+
+export function sqrt(input: {val: number | string | ICharacter | INumeric}) {
+  let num_in: number | undefined = undefined;
+  if (typeof input.val === "number") {
+    num_in = input.val;
+  } else if (typeof input.val === "string") {
+    num_in = parseFloat(input.val);
+  } else if (input.val instanceof Float) {
+    num_in = input.val.getRaw();
+  } else {
+    num_in = parseFloat(input.val.get().toString());
+  }
+  return Math.sqrt(num_in);
+}
