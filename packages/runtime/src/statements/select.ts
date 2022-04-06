94018971a4b4215c0001589b4ce1172415290b7e
Lars Hvam <larshp@hotmail.com>
2022-04-06 15:33:02 +0200

    cleanup

--- packages/runtime/src/statements/select.ts
+++ packages/runtime/src/statements/select.ts
@@ -14,48 +14,45 @@ export class SelectDatabase {
       throw new Error("Runtime, database not initialized");
     }
 
-    const {rows: res} = this.context.db.select({select});
+    const {rows: rows} = this.context.db.select({select});
 
     if (target instanceof FieldSymbol) {
+      if (target.isAssigned() === false) {
+        throw "select, fs not assigned";
+      }
       // @ts-ignore
       target = target.getPointer();
     }
-
     target.clear();
+
+    if (rows.length === 0) {
+      // @ts-ignore
+      abap.builtin.sy.get().subrc.set(4);
+      return;
+    }
+
     if (target instanceof Structure) {
-      if (res.length === 0) {
-        // @ts-ignore
-        abap.builtin.sy.get().subrc.set(4);
-      } else {
-        const result: any = {};
-        for (const column in res[0]) {
-          result[column] = clone(target.get()[column]).set(res[0][column]);
-        }
-        target.set(new Structure(result));
-        // @ts-ignore
-        abap.builtin.sy.get().subrc.set(0);
+      const result: any = {};
+      for (const column in rows[0]) {
+        result[column] = clone(target.get()[column]).set(rows[0][column]);
       }
+      target.set(new Structure(result));
     } else if (target instanceof Table) {
-      if (res.length === 0) {
-        // @ts-ignore
-        abap.builtin.sy.get().subrc.set(4);
-      } else {
-        for (const row of res) {
-          const targetRow = clone(target.getRowType());
-          for (const columnName in row) {
-            // @ts-ignore
-            targetRow.get()[columnName]?.set(row[columnName]);
-          }
-
+      for (const row of rows) {
+        const targetRow = clone(target.getRowType());
+        for (const columnName in row) {
           // @ts-ignore
-          abap.statements.insertInternal({table: target, data: targetRow});
+          targetRow.get()[columnName]?.set(row[columnName]);
         }
 
         // @ts-ignore
-        abap.builtin.sy.get().subrc.set(0);
+        abap.statements.insertInternal({table: target, data: targetRow});
       }
     } else {
       throw new Error("Runtime, SELECT todo");
     }
+
+    // @ts-ignore
+    abap.builtin.sy.get().subrc.set(0);
   }
 }
