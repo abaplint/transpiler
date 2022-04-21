d3a1c4a1cc589cb6dfb24f246b6f54b234af9b4a
Lars Hvam <larshp@hotmail.com>
2022-04-21 11:18:09 +0200

    basic cast

--- packages/runtime/src/statements/cast.ts
+++ packages/runtime/src/statements/cast.ts
@@ -2,10 +2,26 @@ import {initial} from "../compare";
 import {ABAPObject} from "../types";
 
 // todo, field symbols as input?
+// todo, local classes?
 export async function cast(target: ABAPObject, source: ABAPObject) {
   if (initial(source)) {
     target.clear();
     return;
+  }
+
+  const targetName = target.getQualifiedName();
+
+  // using "instanceof" is probably wrong in some cases,
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
+  // @ts-ignore
+  if (abap.Classes[targetName] && source instanceof abap.Classes[targetName] === false) {
+    // @ts-ignore
+    if (abap.Classes["CX_SY_MOVE_CAST_ERROR"] !== undefined) {
+      // @ts-ignore
+      throw new abap.Classes["CX_SY_MOVE_CAST_ERROR"]();
+    } else {
+      throw "Global class CX_SY_MOVE_CAST_ERROR not found";
+    }
   }
 
 // throw cx_sy_move_cast_error if incompatible
