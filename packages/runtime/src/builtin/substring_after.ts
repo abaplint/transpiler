06874d7ae794400a916d077aaea794cf49e9a500
Lars Hvam <larshp@hotmail.com>
2022-04-26 18:34:10 +0200

    2.0.12, CAST, workaround (#717)
    * CAST, workaround
    
    * substring_after, add regex
    
    * 2.0.12
    
    * disable cast
    
    * 2.0.13

--- packages/runtime/src/builtin/substring_after.ts
+++ packages/runtime/src/builtin/substring_after.ts
@@ -1,11 +1,20 @@
 import {ICharacter} from "../types/_character";
 import {String} from "../types/string";
 
-export function substring_after(input: {val: ICharacter | string, sub: ICharacter | string}): ICharacter {
+export function substring_after(input: {val: ICharacter | string, sub?: ICharacter | string, regex?: ICharacter | string}): ICharacter {
   const val = typeof input.val === "string" ? input.val : input.val.get();
-  const s = typeof input.sub === "string" ? input.sub : input.sub.get();
+  let reg = "";
+  if (typeof input.regex === "string") {
+    reg = input.regex;
+  } else if (input?.regex) {
+    reg = input.regex.get();
+  } else if (typeof input.sub === "string") {
+    reg = input.sub;
+  } else if (input?.sub) {
+    reg = input.sub.get();
+  }
 
-  const r = new RegExp(s + "(.*)");
+  const r = new RegExp(reg + "(.*)");
   const res = val.match(r);
 
   let ret = "";
