f5ba53ec82013945c302414595eda3bc6ae97396
Lars Hvam <larshp@hotmail.com>
2022-05-01 15:20:27 +0200

    implement translate()

---
+++ test/builtin/translate.ts
@@ -0,0 +1,57 @@
+// import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Builtin functions - translate", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("translate() 1", async () => {
+    const code = `
+DATA foo TYPE string.
+foo = translate( val = |foo| from = |o| to = |u| ).
+ASSERT foo = 'fuu'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+  it("translate() 2", async () => {
+    const code = `
+DATA foo TYPE string.
+foo = translate( val = |foo| from = |o| to = || ).
+ASSERT foo = 'f'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+  it("translate() 3", async () => {
+    const code = `
+DATA foo TYPE string.
+foo = translate( val = |foo| from = |fo| to = |ba| ).
+ASSERT foo = 'baa'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+  it("translate(), empty", async () => {
+    const code = `
+DATA foo TYPE string.
+foo = translate( val = |foo| from = || to = || ).
+ASSERT foo = 'foo'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
