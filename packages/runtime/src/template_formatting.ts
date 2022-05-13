8105ab832a78e8962dfc2fa8f2c47be54d45060c
Lars Hvam <larshp@hotmail.com>
2022-05-13 11:09:54 +0200

    2.0.25, bugfix, packed and DECIMALS (#736)

--- packages/runtime/src/template_formatting.ts
+++ packages/runtime/src/template_formatting.ts
@@ -1,4 +1,4 @@
-import {Integer} from "./types";
+import {Integer, Packed} from "./types";
 import {ICharacter} from "./types/_character";
 import {INumeric} from "./types/_numeric";
 
@@ -37,6 +37,8 @@ export function templateFormatting(source: ICharacter | INumeric, options: optio
     text = text.trimEnd().padEnd(options.width, " ");
   } else if (options.decimals && source instanceof Integer) {
     text = source.get() + "." + "".padEnd(options.decimals, "0");
+  } else if (options.decimals && source instanceof Packed) {
+    text = source.get().toFixed(options.decimals);
   }
   return text;
 }
