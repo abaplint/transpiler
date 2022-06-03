146deaaf7dd6a29fab5d57cba02b2f12ac16dc78
Lars Hvam <larshp@hotmail.com>
2022-06-03 14:45:15 +0200

    update

--- packages/runtime/src/builtin/boolc.ts
+++ packages/runtime/src/builtin/boolc.ts
@@ -4,9 +4,9 @@ export function boolc(input: boolean | undefined | {val: Character}) {
   if (input === true) {
     return new String().set("X");
   } else if (input === false || input === undefined) {
-    return new String().set("");
+    return new String().set(" ");
   } else if (input.val instanceof Character && input.val.get().trim() === "") {
-    return new String().set("");
+    return new String().set(" ");
   } else {
     return new String().set("X");
   }
