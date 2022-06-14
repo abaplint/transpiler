406601050ec09bdca286d51c41e5083e7df5d374
Lars Hvam <larshp@hotmail.com>
2022-06-14 17:43:44 +0200

    refactoring

--- packages/runtime/src/statements/write.ts
+++ packages/runtime/src/statements/write.ts
@@ -27,27 +27,26 @@ export class WriteStatement {
       if (options?.newLine === true && this.context.console.get().length > 0) {
         this.context.console.add("\n");
       }
+
       if (typeof source === "string" || typeof source === "number") {
         if (options?.target) {
           options.target.set(source.toString());
         } else {
           this.context.console.add(source.toString());
         }
+      } else if (source instanceof Structure) {
+        const obj = source.get();
+        for (const f in obj) {
+          this.write(obj[f], {...options});
+        }
       } else {
-        if (source instanceof Structure) {
-          const obj = source.get();
-          for (const f in obj) {
-            // @ts-ignore
-            abap.statements.write(obj[f]);
-          }
+        if (options?.target) {
+          options.target.set(source.get().toString());
         } else {
-          if (options?.target) {
-            options.target.set(source.get().toString());
-          } else {
-            this.context.console.add(source.get().toString());
-          }
+          this.context.console.add(source.get().toString());
         }
       }
+
     }
   }
 }
