5bd481ce3188aa435fa67b18e105a7eb8adcb24b
Lars Hvam <larshp@hotmail.com>
2022-06-15 07:29:14 +0200

    bugfix, string addition

--- packages/runtime/src/operators/add.ts
+++ packages/runtime/src/operators/add.ts
@@ -1,19 +1,28 @@
 import {Float, Hex, Integer} from "../types";
 import {ICharacter} from "../types/_character";
 import {INumeric} from "../types/_numeric";
+import {String} from "../types/string";
 import {parse} from "./_parse";
 
 export function add(left: INumeric | ICharacter | string | number | Float | Integer | Hex,
                     right: INumeric | ICharacter | string | number  | Float | Integer | Hex) {
+
   if (left instanceof Integer && right instanceof Integer) {
     return new Integer().set(left.get() + right.get());
   } else if (typeof left === "number" && typeof right === "number"
       && Number.isInteger(left) && Number.isInteger(right)) {
     return new Integer().set(left + right);
+
   } else if (typeof left === "number" && Number.isInteger(left) && right instanceof Integer) {
     return new Integer().set(left + right.get());
   } else if (typeof right === "number" && Number.isInteger(right) && left instanceof Integer) {
     return new Integer().set(left.get() + right);
+
+  } else if (left instanceof String && Number.isInteger(Number(left.get())) && right instanceof Integer) {
+    return new Integer().set(Number.parseInt(left.get(), 10) + right.get());
+  } else if (right instanceof String && Number.isInteger(Number(right)) && left instanceof Integer) {
+    return new Integer().set(left.get() + Number.parseInt(right.get(), 10));
   }
+
   return new Float().set(parse(left) + parse(right));
 }
--- packages/runtime/src/operators/multiply.ts
+++ packages/runtime/src/operators/multiply.ts
@@ -2,6 +2,7 @@ import {Float, Integer} from "../types";
 import {ICharacter} from "../types/_character";
 import {INumeric} from "../types/_numeric";
 import {parse} from "./_parse";
+import {String} from "../types/string";
 
 export function multiply(left: INumeric | ICharacter | string | number, right: INumeric | ICharacter | string | number) {
   if (left instanceof Integer && right instanceof Integer) {
@@ -9,10 +10,17 @@ export function multiply(left: INumeric | ICharacter | string | number, right: I
   } else if (typeof left === "number" && typeof right === "number"
       && Number.isInteger(left) && Number.isInteger(right)) {
     return new Integer().set(left * right);
+
   } else if (typeof left === "number" && Number.isInteger(left) && right instanceof Integer) {
     return new Integer().set(left * right.get());
   } else if (typeof right === "number" && Number.isInteger(right) && left instanceof Integer) {
     return new Integer().set(left.get() * right);
+
+  } else if (left instanceof String && Number.isInteger(Number(left.get())) && right instanceof Integer) {
+    return new Integer().set(Number.parseInt(left.get(), 10) * right.get());
+  } else if (right instanceof String && Number.isInteger(Number(right)) && left instanceof Integer) {
+    return new Integer().set(left.get() * Number.parseInt(right.get(), 10));
   }
+
   return new Float().set(parse(left) * parse(right));
 }
