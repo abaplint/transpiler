fc1c4166e54a87cb1dcbb9815b8043202ff07ef6
Lars Hvam <larshp@hotmail.com>
2022-08-02 15:56:27 +0200

    enable casting logic

--- packages/runtime/src/statements/cast.ts
+++ packages/runtime/src/statements/cast.ts
@@ -3,33 +3,35 @@ import {ABAPObject} from "../types";
 
 // todo, field symbols as input?
 // todo, local classes?
+// check with javascript instanceof?
+// handling interfaces?
 export async function cast(target: ABAPObject, source: ABAPObject) {
   if (initial(source)) {
     target.clear();
     return;
   }
 
-  /*
-  const targetName = target.getQualifiedName()?.toUpperCase();
-  if (targetName?.startsWith("IF_") === false
-      && targetName?.startsWith("ZIF_") === false) { // todo, interfaces are also classes but not inherited
-    // using "instanceof" is probably wrong in some cases,
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
-    // @ts-ignore
-    if (abap.Classes[targetName] && source instanceof abap.Classes[targetName] === false) {
+  // eslint-disable-next-line prefer-const
+  let castEnabled = true;
+  if (castEnabled === true) {
+    const targetName = target.getQualifiedName()?.toUpperCase();
+    if (targetName?.startsWith("IF_") === false
+        && targetName?.startsWith("ZIF_") === false) { // todo, interfaces are also classes but not inherited
+      // using "instanceof" is probably wrong in some cases,
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
       // @ts-ignore
-      if (abap.Classes["CX_SY_MOVE_CAST_ERROR"] !== undefined) {
+      if (abap.Classes[targetName] && source.get() instanceof abap.Classes[targetName] === false) {
         // @ts-ignore
-        throw new abap.Classes["CX_SY_MOVE_CAST_ERROR"]();
-      } else {
-        throw "Global class CX_SY_MOVE_CAST_ERROR not found";
+        if (abap.Classes["CX_SY_MOVE_CAST_ERROR"] !== undefined) {
+          // @ts-ignore
+          throw new abap.Classes["CX_SY_MOVE_CAST_ERROR"]();
+        } else {
+          throw "Global class CX_SY_MOVE_CAST_ERROR not found";
+        }
       }
     }
+    target.set(source);
+  } else {
+    target.set(source);
   }
-  */
-
-// throw cx_sy_move_cast_error if incompatible
-// check with javascript instanceof?
-// handling interfaces?
-  target.set(source);
 }
