e37184c13c3a05ccfca1bf7de00e2b83580afb2f
Lars Hvam <larshp@hotmail.com>
2022-08-10 07:42:51 +0200

    2.1.26, insert into REPOSRC (#834)

--- packages/transpiler/src/db/index.ts
+++ packages/transpiler/src/db/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import * as abaplint from "@abaplint/core";
 import {DatabaseSetupResult} from "./database_setup_result";
 import {SQLiteDatabaseSchema} from "./sqlite_database_schema";
@@ -27,38 +28,59 @@ export class DatabaseSetup {
     // INSERT data
     for (const obj of this.reg.getObjects()) {
       if (obj instanceof abaplint.Objects.MessageClass) {
-        insert += this.messageClass(obj);
+        insert += this.insertT100(obj);
+      } else if (obj instanceof abaplint.Objects.Class
+          || obj instanceof abaplint.Objects.Interface) {
+        insert += this.insertREPOSRC(obj);
       }
     }
-    insert += this.t000Insert();
+    insert += this.insertT000();
     return insert;
   }
 
-  private t000Insert(): string {
-    const obj = this.reg.getObject("TABL", "T000") as abaplint.Objects.Table | undefined;
-    if (obj === undefined) {
+  private insertREPOSRC(obj: abaplint.ABAPObject): string {
+    if (this.reg.getObject("TABL", "REPOSRC") === undefined) {
       return "";
     }
 
-    const type = obj.parseType(this.reg);
-    if (type instanceof abaplint.BasicTypes.StructureType && type.getComponents().length === 3) {
+    const name = obj.getName().toUpperCase();
+    const raw = obj.getMainABAPFile()?.getRaw();
+    if (raw === undefined) {
+      return "";
+    }
+
+    return `INSERT INTO reposrc ('PROGNAME', 'DATA') VALUES ('${name}', '${this.escape(raw)}');\n`;
+  }
+
+  private insertT000(): string {
+    const tabl = this.reg.getObject("TABL", "T000") as abaplint.Objects.Table | undefined;
+    if (tabl === undefined) {
+      return "";
+    }
+
+    const type = tabl.parseType(this.reg);
+    if (type instanceof abaplint.BasicTypes.StructureType && type.getComponents().length >= 3) {
       // todo, this should take the client number from the settings
-      return `INSERT INTO t000 VALUES ('123', '', '');\n`;
+      return `INSERT INTO t000 ('MANDT', 'CCCATEGORY', 'CCNOCLIIND') VALUES ('123', '', '');\n`;
     } else {
       return "";
     }
   }
 
-  private messageClass(msag: abaplint.Objects.MessageClass): string {
+  private insertT100(msag: abaplint.Objects.MessageClass): string {
     // ignore if T100 is unknown
     if (this.reg.getObject("TABL", "T100") === undefined) {
       return "";
     }
     let ret = "";
     for (const m of msag.getMessages()) {
-      ret += `INSERT INTO t100 VALUES ('E', '${msag.getName()}', '${m.getNumber()}', '${m.getMessage().replace(/\'/g, "''")}');\n`;
+      ret += `INSERT INTO t100 ('SPRSL', 'ARBGB', 'MSGNR', 'TEXT') VALUES ('E', '${msag.getName()}', '${m.getNumber()}', '${this.escape(m.getMessage())}');\n`;
     }
     return ret;
+  }
+
+  private escape(value: string): string {
+    return value.replace(/\'/g, "''");
   }
 
 }
