d04ca66ce1d6ef8ce130f2c452e208c0f88381ab
Lars Hvam <larshp@hotmail.com>
2022-09-15 10:37:00 +0200

    2.1.66, round() floor (#876)

--- test/builtin/round.ts
+++ test/builtin/round.ts
@@ -14,7 +14,7 @@ describe("Builtin functions - round", () => {
     abap = new ABAP();
   });
 
-  it("test", async () => {
+  it("test, half down", async () => {
     const code = `
     CONSTANTS half_down TYPE i VALUE 4.
     DATA lv_f TYPE f.
@@ -27,12 +27,31 @@ describe("Builtin functions - round", () => {
     WRITE / lv_num.
     lv_f = '2.7'.
     lv_num = round( val = lv_f dec = 0 mode = half_down ).
-    WRITE / lv_num.
-      `;
+    WRITE / lv_num.`;
     const js = await run(code);
     const f = new AsyncFunction("abap", js);
     await f(abap);
     expect(abap.console.get()).to.equal( `2\n2\n3`);
+  });
+
+  it("test, floor", async () => {
+    const code = `
+    CONSTANTS floor TYPE i VALUE 6.
+    DATA lv_f TYPE f.
+    DATA lv_num TYPE i.
+    lv_f = '2.1'.
+    lv_num = round( val = lv_f dec = 0 mode = floor ).
+    WRITE / lv_num.
+    lv_f = '2.5'.
+    lv_num = round( val = lv_f dec = 0 mode = floor ).
+    WRITE / lv_num.
+    lv_f = '2.7'.
+    lv_num = round( val = lv_f dec = 0 mode = floor ).
+    WRITE / lv_num.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal( `2\n2\n2`);
   });
 
   it("half should round up", async () => {
