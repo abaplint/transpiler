53e090c3af74346a22d1cd226d88e9684a91b84d
Lars Hvam <larshp@hotmail.com>
2022-09-25 09:39:36 +0200

    fix chained assignment

--- packages/transpiler/src/statements/move.ts
+++ packages/transpiler/src/statements/move.ts
@@ -7,21 +7,27 @@ export class MoveTranspiler implements IStatementTranspiler {
 
   public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): Chunk {
     const source = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Source));
-    const target = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Target));
+
+    const targets: Chunk[] = [];
+    for (const t of node.findDirectExpressions(abaplint.Expressions.Target)) {
+      targets.push(traversal.traverse(t));
+    }
 
     const ret = new Chunk();
     const second = node.getChildren()[1]?.concatTokens();
     if (second === "?=") {
       ret.appendString("await abap.statements.cast(")
-        .appendChunk(target)
+        .appendChunk(targets[0])
         .appendString(", ")
         .appendChunk(source)
         .append(");", node.getLastToken(), traversal);
     } else {
-      ret.appendChunk(target)
-        .appendString(".set(")
-        .appendChunk(source)
-        .append(");", node.getLastToken(), traversal);
+      for (const target of targets) {
+        ret.appendChunk(target)
+          .appendString(".set(")
+          .appendChunk(source)
+          .append(");", node.getLastToken(), traversal);
+      }
     }
 
     return ret;
