76f66e0bb11240d2bf899d4cdbd07f009698b0ef
Lars Hvam <larshp@hotmail.com>
2022-10-05 16:47:21 +0200

    escape table name

--- packages/transpiler/test/db.ts
+++ packages/transpiler/test/db.ts
@@ -11,7 +11,7 @@ describe("transpiler, database setup", () => {
     const tabl = new abaplint.MemoryFile("t100.tabl.xml", tabl_t100xml);
     const reg = new abaplint.Registry().addFile(tabl).parse();
     const result = new SQLiteDatabaseSchema(reg).run();
-    expect(result[0]).to.equal(`CREATE TABLE t100 ('sprsl' NCHAR(1), 'arbgb' NCHAR(20), 'msgnr' NCHAR(3), 'text' NCHAR(73), PRIMARY KEY('sprsl','arbgb','msgnr'));`);
+    expect(result[0]).to.equal(`CREATE TABLE 't100' ('sprsl' NCHAR(1), 'arbgb' NCHAR(20), 'msgnr' NCHAR(3), 'text' NCHAR(73), PRIMARY KEY('sprsl','arbgb','msgnr'));`);
   });
 
   it("t100 create table, insert MSAG entries", async () => {
@@ -26,7 +26,7 @@ describe("transpiler, database setup", () => {
     const view = new abaplint.MemoryFile("zag_unit_test_v.view.xml", zag_unit_test_v);
     const reg = new abaplint.Registry().addFiles([view]).parse();
     const result = new SQLiteDatabaseSchema(reg).run();
-    expect(result[0]).to.equal(`CREATE VIEW zag_unit_test_v AS SELECT zag_unit_test_t1.mandt AS mandt, zag_unit_test_t1.key_field AS key_field, zag_unit_test_t1.data_field AS data_field, zag_unit_test_t2.data AS data FROM zag_unit_test_t1 INNER JOIN zag_unit_test_t2 ON zag_unit_test_t1.data_field = zag_unit_test_t2.key_field;`);
+    expect(result[0]).to.equal(`CREATE VIEW 'zag_unit_test_v' AS SELECT zag_unit_test_t1.mandt AS mandt, zag_unit_test_t1.key_field AS key_field, zag_unit_test_t1.data_field AS data_field, zag_unit_test_t2.data AS data FROM zag_unit_test_t1 INNER JOIN zag_unit_test_t2 ON zag_unit_test_t1.data_field = zag_unit_test_t2.key_field;`);
   });
 
 });
