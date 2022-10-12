31f07c79f657ca6afe6f50050a9cc9113427f683
Lars Hvam <larshp@hotmail.com>
2022-10-12 17:50:24 +0200

    2.3.0: ENQU support (#929)

---
+++ packages/transpiler/src/handlers/handle_enqu.ts
@@ -0,0 +1,43 @@
+import * as abaplint from "@abaplint/core";
+import {Chunk} from "../chunk";
+import {IOutputFile} from "../types";
+
+// view, much like the tables
+export class HandleEnqu {
+  public runObject(obj: abaplint.Objects.LockObject, _reg: abaplint.IRegistry): IOutputFile[] {
+
+    const filename = obj.getXMLFile()?.getFilename().replace(".xml", ".mjs").toLowerCase();
+    if (filename === undefined) {
+      return [];
+    }
+
+    const chunk = new Chunk().appendString(`// enqueue object
+abap.FunctionModules["ENQUEUE_${obj.getName().toUpperCase()}_LOCK"] = async (INPUT) => {
+  const lookup = abap.Classes["KERNEL_LOCK"];
+  if (lookup === undefined) {
+    throw new Error("Lock, kernel class missing");
+  }
+  await lookup.enqueue(INPUT);
+};
+abap.FunctionModules["DEQUEUE_${obj.getName().toUpperCase()}_LOCK"] = async (INPUT) => {
+  const lookup = abap.Classes["KERNEL_LOCK"];
+  if (lookup === undefined) {
+    throw new Error("Lock, kernel class missing");
+  }
+  await lookup.dequeue(INPUT);
+};`);
+
+    const output: IOutputFile = {
+      object: {
+        name: obj.getName(),
+        type: obj.getType(),
+      },
+      filename: filename,
+      chunk: chunk,
+      requires: [],
+      exports: [],
+    };
+
+    return [output];
+  }
+}
