8e070dca25f450d6f15094efe7b0749fea58e08b
Lars Hvam <larshp@hotmail.com>
2022-10-14 15:18:59 +0200

    basic COLLECT

--- packages/transpiler/src/statements/collect.ts
+++ packages/transpiler/src/statements/collect.ts
@@ -5,8 +5,22 @@ import {Chunk} from "../chunk";
 
 export class CollectTranspiler implements IStatementTranspiler {
 
-  public transpile(_node: abaplint.Nodes.StatementNode, _traversal: Traversal): Chunk {
-    return new Chunk(`throw new Error("Collect, transpiler todo");`);
+  public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): Chunk {
+
+    const source = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Source));
+    const target = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Target));
+
+    const fstarget = node.findDirectExpression(abaplint.Expressions.FSTarget);
+    if (fstarget) {
+      return new Chunk(`throw new Error("Collect, transpiler todo");`);
+    }
+
+    return new Chunk()
+      .appendString("abap.statements.collect(")
+      .appendChunk(source)
+      .appendString(",")
+      .appendChunk(target)
+      .appendString(");");
   }
 
 }
---
+++ test/statements/collect.ts
@@ -0,0 +1,36 @@
+import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - COLLECT", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("simple", async () => {
+    const code = `
+DATA lt_namespace TYPE STANDARD TABLE OF string WITH DEFAULT KEY.
+DATA lv_namespace LIKE LINE OF lt_namespace.
+lv_namespace = 'foo'.
+COLLECT lv_namespace INTO lt_namespace.
+lv_namespace = 'bar'.
+COLLECT lv_namespace INTO lt_namespace.
+lv_namespace = 'foo'.
+COLLECT lv_namespace INTO lt_namespace.
+LOOP AT lt_namespace INTO lv_namespace.
+  WRITE / lv_namespace.
+ENDLOOP.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("foo\nbar");
+  });
+
+});
