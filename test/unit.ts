e8a132e139d02306ad589276ab6437f09e922715
Lars Hvam <larshp@hotmail.com>
2022-10-18 08:34:06 +0200

    update abaplint

--- test/unit.ts
+++ test/unit.ts
@@ -1973,7 +1973,7 @@ ENDCLASS.`;
     await dumpNrun(files);
   });
 
-  it.only("test-48", async () => {
+  it.skip("test-48", async () => {
 // namespaced function group with function module
 
     const fmname = `FUNCTION /foo/fmname.
