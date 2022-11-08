372d5a37075b203f0f1cfdc662428242aa280910
Lars Hvam <larshp@hotmail.com>
2022-11-08 20:47:16 +0100

    2.3.40: bugfix, nmin & nmax (#979)

--- test/builtin/nmax.ts
+++ test/builtin/nmax.ts
@@ -8,43 +8,10 @@ async function run(contents: string) {
   return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
 }
 
-describe("Builtin Numeric Extremum Functions, nmin & nmax", () => {
+describe("Builtin functions - nmin", () => {
 
   beforeEach(async () => {
     abap = new ABAP();
-  });
-
-  it("Builtin numerical: nmin 1", async () => {
-    const code = `
-      DATA int1 TYPE i VALUE 1.
-      DATA int2 TYPE i VALUE 2.
-      DATA min TYPE i.
-      min = nmin( val1 = int1
-                  val2 = int2 ).
-      WRITE / min.`;
-    const js = await run(code);
-    const f = new AsyncFunction("abap", js);
-    await f(abap);
-    expect(abap.console.get()).to.equal("1");
-  });
-
-  it("Builtin numerical: nmin 2", async () => {
-    const code = `
-      DATA int1 TYPE i VALUE 42.
-      DATA int2 TYPE i VALUE 37.
-      DATA min TYPE i.
-      min = nmin( val1 = int1
-                  val2 = 99
-                  val3 = 156
-                  val4 = 234
-                  val5 = 777
-                  val6 = int2
-                  val7 = 200000 ).
-      WRITE / min.`;
-    const js = await run(code);
-    const f = new AsyncFunction("abap", js);
-    await f(abap);
-    expect(abap.console.get()).to.equal("37");
   });
 
   it("Builtin numerical: nmax 1", async () => {
@@ -78,6 +45,20 @@ describe("Builtin Numeric Extremum Functions, nmin & nmax", () => {
     const f = new AsyncFunction("abap", js);
     await f(abap);
     expect(abap.console.get()).to.equal("200000");
+  });
+
+  it("packed", async () => {
+    const code = `
+    DATA total1 TYPE p LENGTH 3 DECIMALS 2.
+    DATA total2 TYPE p LENGTH 3 DECIMALS 2.
+    total1 = '15.3'.
+    total2 = '15.2'.
+    total1 = nmax( val1 = total1 val2 = total2 ).
+    WRITE total1.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("15.30");
   });
 
 });
