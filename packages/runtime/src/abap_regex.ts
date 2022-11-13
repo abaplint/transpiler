6c3c9af7591541b237462a62e036e9c40c9665d4
Lars Hvam <larshp@hotmail.com>
2022-11-13 11:39:16 +0100

    2.3.41: regex bugfixes (#980)

--- packages/runtime/src/abap_regex.ts
+++ packages/runtime/src/abap_regex.ts
@@ -3,7 +3,14 @@ export class ABAPRegExp {
   // converts from ABAP specific regex to javascript regex
   public static convert(input: string): string {
     let ret = input;
-    ret = input.replace(/\[\[:punct:\]\]/g, "[@%\\.\\,\\-\\{\\}\\[\\]\\:\\!\\?\\(\\)\\;\\']");
+
+    ret = ret.replace(/\[\[:punct:\]\]/g, "[@%\\.\\,\\-\\{\\}\\[\\]\\:\\!\\?\\(\\)\\;\\']");
+
+    // https://github.com/micromatch/posix-character-classes#posix-character-classes
+    ret = ret.replace(/\[\^\[:print:\]\]/g, "[\\x00-\\x1F\\x7F]");
+
+    ret = ret.replace("[[:space:]]", "\\s");
+
     return ret;
   }
 }
