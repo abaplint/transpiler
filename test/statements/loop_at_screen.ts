abe39db05da874042cc443e41a04a52724557809
Lars Hvam <larshp@hotmail.com>
2022-12-07 09:11:28 +0100

    2.3.80: LOOP AT SCREEN scaffolding (#1021)

---
+++ test/statements/loop_at_screen.ts
@@ -0,0 +1,26 @@
+import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - LOOP AT SCREEN", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("Basic", async () => {
+    const code = `
+LOOP AT SCREEN.
+ENDLOOP.`;
+    const js = await run(code);
+    expect(js).to.include("{");
+    expect(js).to.include("}");
+  });
+
+});
