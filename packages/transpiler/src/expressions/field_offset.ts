97eb87ec54e02130648097a6efa9af22a0d56f4f
Lars Hvam <larshp@hotmail.com>
2023-01-08 11:49:54 +0100

    fix

--- packages/transpiler/src/expressions/field_offset.ts
+++ packages/transpiler/src/expressions/field_offset.ts
@@ -1,8 +1,8 @@
 import {Expressions, Nodes, Tokens} from "@abaplint/core";
 import {IExpressionTranspiler} from "./_expression_transpiler";
 import {Traversal} from "../traversal";
-import {FieldSymbolTranspiler} from "./field_symbol";
 import {Chunk} from "../chunk";
+import {FieldChainTranspiler} from "./field_chain";
 
 export class FieldOffsetTranspiler implements IExpressionTranspiler {
 
@@ -10,23 +10,8 @@ export class FieldOffsetTranspiler implements IExpressionTranspiler {
     let ret = "";
     for (const c of node.getChildren()) {
       if (c instanceof Nodes.ExpressionNode) {
-        if (c.get() instanceof Expressions.SourceField) {
-          let sourceStr = c.getFirstToken().getStr().toLowerCase();
-          if (sourceStr === "sy") {
-            sourceStr = "abap.builtin.sy";
-          }
-          ret += sourceStr;
-        } else if (c.get() instanceof Expressions.SourceFieldSymbol) {
-          ret += new FieldSymbolTranspiler().transpile(c, traversal).getCode();
-        } else if (c.get() instanceof Expressions.ArrowOrDash) {
-          ret += ".get()";
-        } else if (c.get() instanceof Expressions.ComponentName) {
-          const name = c.getFirstToken().getStr().toLowerCase();
-          if (name.match(/^\d/)) {
-            ret += `["` + name + `"]`;
-          } else {
-            ret += `.` + name;
-          }
+        if (c.get() instanceof Expressions.SimpleFieldChain2) {
+          ret = new FieldChainTranspiler().transpile(c, traversal).getCode();
         }
       } else if(c instanceof Nodes.TokenNode) {
         if (c.get() instanceof Tokens.Identifier) {
