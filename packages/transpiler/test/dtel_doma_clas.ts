dfe14ecddc4213646abcc2f2e40cd2b07d765d67
Lars Hvam <larshp@hotmail.com>
2023-01-20 08:23:06 +0100

    hmm

--- packages/transpiler/test/dtel_doma_clas.ts
+++ packages/transpiler/test/dtel_doma_clas.ts
@@ -20,7 +20,7 @@ describe("DTEL + DOMA + CLAS", () => {
    <DD04V>
     <ROLLNAME>AMOO</ROLLNAME>
     <DDLANGUAGE>E</DDLANGUAGE>
-    <DOMNAME>CHAR1</DOMNAME>
+    <DOMNAME>FLAG</DOMNAME>
     <HEADLEN>10</HEADLEN>
     <SCRLEN1>10</SCRLEN1>
     <SCRLEN2>10</SCRLEN2>
@@ -38,22 +38,22 @@ describe("DTEL + DOMA + CLAS", () => {
 </abapGit>`;
     const file1 = {filename: "amoo.dtel.xml", contents: amoo_dtel};
 
-    const char1_doma = `<?xml version="1.0" encoding="utf-8"?>
+    const flag_doma = `<?xml version="1.0" encoding="utf-8"?>
 <abapGit version="v1.0.0" serializer="LCL_OBJECT_DOMA" serializer_version="v1.0.0">
  <asx:abap xmlns:asx="http://www.sap.com/abapxml" version="1.0">
   <asx:values>
    <DD01V>
-    <DOMNAME>CHAR1</DOMNAME>
+    <DOMNAME>FLAG</DOMNAME>
     <DDLANGUAGE>E</DDLANGUAGE>
     <DATATYPE>CHAR</DATATYPE>
     <LENG>000001</LENG>
     <OUTPUTLEN>000001</OUTPUTLEN>
-    <DDTEXT>CHAR1</DDTEXT>
+    <DDTEXT>FOO</DDTEXT>
    </DD01V>
   </asx:values>
  </asx:abap>
 </abapGit>`;
-    const file2 = {filename: "char1.doma.xml", contents: char1_doma};
+    const file2 = {filename: "flag.doma.xml", contents: flag_doma};
 
     const char1_dtel = `<?xml version="1.0" encoding="utf-8"?>
 <abapGit version="v1.0.0" serializer="LCL_OBJECT_DTEL" serializer_version="v1.0.0">
@@ -62,7 +62,7 @@ describe("DTEL + DOMA + CLAS", () => {
    <DD04V>
     <ROLLNAME>CHAR1</ROLLNAME>
     <DDLANGUAGE>E</DDLANGUAGE>
-    <DOMNAME>CHAR1</DOMNAME>
+    <DOMNAME>FLAG</DOMNAME>
     <DDTEXT>CHAR1</DDTEXT>
     <DTELMASTER>D</DTELMASTER>
     <REFKIND>D</REFKIND>
