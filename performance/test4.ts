7796084d2fb336ac6bac1cd5ba82e2bb6d4d4c43
Lars Hvam <larshp@hotmail.com>
2023-01-29 17:07:10 +0100

    more performance

---
+++ performance/test4.ts
@@ -0,0 +1,22 @@
+export const test4 = `
+TYPES: BEGIN OF ty,
+         name TYPE string,
+       END OF ty.
+TYPES tt TYPE SORTED TABLE OF ty WITH NON-UNIQUE KEY name.
+DATA tab1 TYPE tt.
+DATA tab2 TYPE tt.
+DATA row LIKE LINE OF tab1.
+DATA numc TYPE n LENGTH 5.
+DATA lc_rows TYPE i VALUE 1000.
+
+DO lc_rows TIMES.
+  numc = sy-index.
+  row-name = 'foo' && numc.
+  INSERT row INTO TABLE tab1.
+ENDDO.
+
+DO 20 TIMES.
+  tab2 = tab1.
+ENDDO.
+
+ASSERT lines( tab2 ) = lc_rows.`;
