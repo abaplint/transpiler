6736a83da185b6b70dea8cd78ff9efd28de927b0
Lars Hvam <larshp@hotmail.com>
2023-01-29 10:36:57 +0100

    add performance test (#1071)

--- performance/index.ts
+++ performance/index.ts
@@ -5,6 +5,7 @@ import * as fs from "fs";
 import * as path from "path";
 import {test1} from "./test1";
 import {test2} from "./test2";
+import {test3} from "./test3";
 
 // NOTES
 // * does not run via Mocha
@@ -16,36 +17,37 @@ async function run(contents: string) {
   return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
 }
 
-type Tests = {name: string, abap: string}[];
-type Results = {name: string, runtime: number}[];
+type Test = {name: string, abap: string};
+type Tests = Test[];
+type Result = {name: string, runtime: number};
+type Results = Result[];
 
 const tests: Tests = [
   {name: "Internal table, APPEND and DELETE", abap: test1},
   {name: "Internal table, READ TABLE, table_line", abap: test2},
+  {name: "Internal table, LOOP USING KEY", abap: test3},
 ];
 
-async function execute() {
-  const result: Results = [];
-  for (const t of tests) {
-    const js = await run(t.abap);
-    const f = new AsyncFunction("abap", js);
+async function execute(t: Test) {
+  const js = await run(t.abap);
+  const f = new AsyncFunction("abap", js);
 
-    const t0 = performance.now();
-    await f(abap);
-    const t1 = performance.now();
+  const t0 = performance.now();
+  await f(abap);
+  const t1 = performance.now();
 
-    result.push({name: t.name, runtime: Math.round(t1 - t0)});
-  }
-  return result;
+  return {name: t.name, runtime: Math.round(t1 - t0)};
 }
 
 async function start() {
+  const results: Results = [];
   const time = new Date().toLocaleTimeString([], {hour: "2-digit", minute: "2-digit", hour12: false}) + " (" + Intl.DateTimeFormat().resolvedOptions().timeZone + ")";
   console.log("START RUNTIME PERFORMANCE TEST, " + time);
-  const results = await execute();
-  let index = 1;
-  for (const r of results) {
-    console.log(`${ (index++ + "").padStart(3, "0") }: ${ r.name.padEnd(50, " ") } ${ r.runtime }ms`);
+  for (const t of tests) {
+    const result = await execute(t);
+    results.push(result);
+    let index = 1;
+    console.log(`${ (index++ + "").padStart(3, "0") }: ${ result.name.padEnd(50, " ") } ${ result.runtime }ms`);
   }
   fs.writeFileSync(__dirname + path.sep + "results.json", JSON.stringify(results, null, 2));
 }
