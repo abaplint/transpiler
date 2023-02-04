4843412809e00c2b131f9010a79bbdd743097639
Lars Hvam <larshp@hotmail.com>
2023-02-04 12:02:25 +0100

    2.4.7: SELECT, dynamic fixes (#1085)

---
+++ packages/runtime/src/expand_dynamic.ts
@@ -0,0 +1,18 @@
+import {FieldSymbol} from "./types";
+
+export function expandDynamic(code: string, ev: (name: string) => FieldSymbol | undefined) {
+  if (code === "") {
+    return "1 = 1";
+  } else {
+// todo more here, this is just one simple case,
+    const match = code.match(/ <(\w+)>/);
+    if (match && match[1]) {
+      const name = "fs_" + match[1] + "_";
+      const found = ev(name);
+      if (found instanceof FieldSymbol) {
+        code = code.replace(/ <(\w+)>/, "'" + found.get() + "'");
+      }
+    }
+    return code;
+  }
+}
