27965db6d78194423e5aecc2176e6c2418126fe3
Lars Hvam <larshp@hotmail.com>
2023-02-14 17:01:22 +0100

    IN CP

--- packages/runtime/src/compare/in.ts
+++ packages/runtime/src/compare/in.ts
@@ -1,6 +1,7 @@
 import {Table} from "../types";
 import {ICharacter} from "../types/_character";
 import {INumeric} from "../types/_numeric";
+import {cp} from "./cp";
 import {eq} from "./eq";
 
 export function compareIn(left: number | string | ICharacter | INumeric, right: Table): boolean {
@@ -11,7 +12,10 @@ export function compareIn(left: number | string | ICharacter | INumeric, right: 
   for (const row of right.array()) {
     if (eq(row.get()["sign"], "I") && eq(row.get()["option"], "EQ")) {
       return eq(row.get()["low"], left);
+    } else if (eq(row.get()["sign"], "I") && eq(row.get()["option"], "CP")) {
+      return cp(left, row.get()["low"]);
     } else {
+      console.dir(row);
       throw "compareIn todo";
     }
   }
