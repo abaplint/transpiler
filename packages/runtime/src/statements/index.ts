c651f9429d73b83803184ce08ab65f02137616a2
Lars Hvam <larshp@hotmail.com>
2023-02-28 12:16:38 +0100

    2.5.7: READ REPORT scaffolding (#1111)

--- packages/runtime/src/statements/index.ts
+++ packages/runtime/src/statements/index.ts
@@ -14,6 +14,7 @@ import {collect} from "./collect";
 import {overlay} from "./overlay";
 import {cast} from "./cast";
 import {getBit} from "./get_bit";
+import {readReport} from "./read_report";
 import {raiseEvent} from "./raise_event";
 import {getLocale} from "./get_locale";
 import {getParameter} from "./get_parameter";
@@ -65,6 +66,7 @@ export class Statements {
   public describe = describe;
   public find = find;
   public getBit = getBit;
+  public readReport = readReport;
   public getLocale = getLocale;
   public getParameter = getParameter;
   public getRunTime = getRunTime;
