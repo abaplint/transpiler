7d3203fc89bd2f4e1bbf3f643e0e2f88488eb406
Lars Hvam <larshp@hotmail.com>
2023-03-03 17:54:14 +0100

    2.5.15: bugfix, default parameter value from INTF (#1118)

--- packages/transpiler/src/expressions/field_chain.ts
+++ packages/transpiler/src/expressions/field_chain.ts
@@ -13,9 +13,10 @@ export class FieldChainTranspiler implements IExpressionTranspiler {
     this.addGet = addGet;
   }
 
-  public transpile(node: Nodes.ExpressionNode, traversal: Traversal, prefix = true, filename?: string): Chunk {
+  public transpile(node: Nodes.ExpressionNode, traversal: Traversal, prefix = true, filename?: string, wrongScope = false): Chunk {
     const ret = new Chunk();
     const extra: string[] = [];
+    let interfaceNameAdded = false;
 
     for (const c of node.getChildren()) {
       if (c.get() instanceof Expressions.SourceField
@@ -27,10 +28,14 @@ export class FieldChainTranspiler implements IExpressionTranspiler {
       } else if (c instanceof Nodes.ExpressionNode && c.get() instanceof Expressions.ClassName) {
         const name = traversal.lookupClassOrInterface(c.getFirstToken().getStr(), c.getFirstToken());
         ret.append(name + ".", c, traversal);
+        if (wrongScope === true && traversal.reg.getObject("INTF", c.getFirstToken().getStr())) {
+          ret.append(Traversal.escapeNamespace(c.getFirstToken().getStr().toLocaleLowerCase())! + "$", c, traversal);
+          interfaceNameAdded = true;
+        }
       } else if (c.get() instanceof Expressions.AttributeName) {
         const interfaceName = traversal.isInterfaceAttribute(c.getFirstToken());
         let name = c.getFirstToken().getStr()!.toLowerCase();
-        if (prefix && interfaceName && name.startsWith(interfaceName) === false) {
+        if (prefix && interfaceName && name.startsWith(interfaceName) === false && interfaceNameAdded === false) {
           name = Traversal.escapeNamespace(name)!.replace("~", "$");
           name = Traversal.escapeNamespace(interfaceName) + "$" + name;
         } else {
