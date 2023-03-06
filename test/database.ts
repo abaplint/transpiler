a5f2a2a31ad73656bb1661cd0ced3b1ee7058cd6
Lars Hvam <larshp@hotmail.com>
2023-03-06 09:03:11 +0100

    wip

--- test/database.ts
+++ test/database.ts
@@ -587,15 +587,19 @@ ASSERT sy-subrc = 0.`;
   it("MODIFY simple", async () => {
     const code = `
     DATA ls_t100 TYPE t100.
+    ls_t100-text = 'foo'.
     MODIFY t100 FROM ls_t100.
-    MODIFY t100 FROM ls_t100.`;
+    WRITE sy-subrc.
+    ls_t100-text = 'bar'.
+    MODIFY t100 FROM ls_t100.
+    WRITE sy-subrc.`;
     const js = await runFiles(abap, [
       {filename: "zfoobar.prog.abap", contents: code},
       {filename: "t100.tabl.xml", contents: tabl_t100xml},
       {filename: "zag_unit_test.msag.xml", contents: msag_zag_unit_test}]);
     const f = new AsyncFunction("abap", js);
     await f(abap);
-//    expect(abap.console.get()).to.equal("2");
+    expect(abap.console.get()).to.equal("00");
   });
 
   it("INSERT FROM TABLE", async () => {
