c03e22b27c9a05954d12bdca4541fa9c0603cfee
Lars Hvam <larshp@hotmail.com>
2023-03-09 15:26:49 +0100

    2.5.22: bugfix, set string (#1125)

---
+++ test/types/string.ts
@@ -0,0 +1,47 @@
+import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running Examples - string type", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("set from field symbol", async () => {
+    const code = `
+    DATA char3 TYPE c LENGTH 3.
+    DATA str TYPE string.
+    FIELD-SYMBOLS <fs> LIKE char3.
+    ASSIGN char3 TO <fs>.
+    str = <fs>.
+    str = '"' && str && '"'.
+    WRITE str.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal(`""`);
+  });
+
+  it("set from integer field symbol", async () => {
+    const code = `
+    DATA foo TYPE i VALUE 2.
+    DATA str TYPE string.
+    FIELD-SYMBOLS <fs> LIKE foo.
+    ASSIGN foo TO <fs>.
+    str = <fs>.
+    str = '"' && str && '"'.
+    WRITE str.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal(`"2 "`);
+  });
+
+});
