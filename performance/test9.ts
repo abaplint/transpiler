5399c840df5d840c90356ec927dec81bf614455e
Lars Hvam <larshp@hotmail.com>
2023-03-19 12:07:46 +0100

    add performance test

---
+++ performance/test9.ts
@@ -0,0 +1,13 @@
+export const test9 = `
+TYPES: BEGIN OF ty,
+         field TYPE string,
+         val   TYPE i,
+       END OF ty.
+DATA tab TYPE HASHED TABLE OF ty WITH UNIQUE KEY field.
+DATA row LIKE LINE OF tab.
+CONSTANTS lc_count TYPE i VALUE 2000.
+DO lc_count TIMES.
+  row-field = |foo{ sy-index }|.
+  INSERT row INTO TABLE tab.
+ENDDO.
+ASSERT lines( tab ) = lc_count.`;
