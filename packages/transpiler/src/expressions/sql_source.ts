83023fb8e930eab56c3428e7528db4a8b32a709e
Lars Hvam <larshp@hotmail.com>
2023-03-21 18:12:48 +0100

    fix

--- packages/transpiler/src/expressions/sql_source.ts
+++ packages/transpiler/src/expressions/sql_source.ts
@@ -8,6 +8,17 @@ export class SQLSourceTranspiler implements IExpressionTranspiler {
 
   public transpile(node: Nodes.ExpressionNode, traversal: Traversal): Chunk {
     let s = node.findDirectExpression(abaplint.Expressions.Source);
+    if (s?.getChildren().length === 1 && s.getChildren()[0].get() instanceof abaplint.Expressions.SQLAliasField) {
+      const chunk = new Chunk();
+      let concat = s.concatTokens();
+      if (concat.includes("~") && concat.split("~")[0].includes("/")) {
+        concat = "'" + concat.replace("~", "'~");
+      }
+      concat = concat.replace("~", ".");
+      chunk.appendString(concat);
+      return chunk;
+    }
+
     if (s === undefined) {
       s = node.findDirectExpression(abaplint.Expressions.SimpleSource3);
     }
