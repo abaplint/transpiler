75d3254b6255a980ac307292373f23bea8e01b57
Lars Hvam <larshp@hotmail.com>
2023-04-06 15:42:03 +0200

    add testcase

--- test/code_structure/classic_exceptions.ts
+++ test/code_structure/classic_exceptions.ts
@@ -129,4 +129,30 @@ START-OF-SELECTION.
     await f(abap);
   });
 
+  it.only("Classic exceptions, raise, CALL METHOD", async () => {
+    const code = `
+CLASS lcl DEFINITION.
+  PUBLIC SECTION.
+    CLASS-METHODS send
+      EXCEPTIONS
+        error.
+ENDCLASS.
+
+CLASS lcl IMPLEMENTATION.
+  METHOD send.
+    RAISE error.
+  ENDMETHOD.
+ENDCLASS.
+
+START-OF-SELECTION.
+  call method lcl=>send
+    exceptions
+      error  = 1
+      OTHERS = 5.
+  ASSERT sy-subrc = 1.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
 });
