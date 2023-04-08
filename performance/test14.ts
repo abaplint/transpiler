851ea985eef7506c431403f4671dd8b3424ae56f
Lars Hvam <larshp@hotmail.com>
2023-04-08 14:44:43 +0200

    add performance test

---
+++ performance/test14.ts
@@ -0,0 +1,9 @@
+export const test14 = `
+DATA: BEGIN OF data,
+        field1 TYPE string,
+        field2 TYPE string,
+      END OF data.
+DO 50000 TIMES.
+  data-field1 = 'FOO'.
+  data-field2 = 'BAR'.
+ENDDO.`;
