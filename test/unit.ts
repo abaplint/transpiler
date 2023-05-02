563997a8fb732d786a5394f9dfd5e26f0a587609
Lars Hvam <larshp@hotmail.com>
2023-05-02 17:33:11 +0200

    fix?

--- test/unit.ts
+++ test/unit.ts
@@ -260,15 +260,15 @@ export async function setup(abap, schemas, insert) {
     // hacks
     output.unitTestScript = output.unitTestScript.replace(
       `import runtime from "@abaplint/runtime";`,
-      `import runtime from "../../packages/runtime/build/src/index.js";`);
+      `import * as runtime from "../../packages/runtime/build/src/index.js";`);
     fs.writeFileSync(outputFolder + path.sep + "index.mjs", output.unitTestScript);
     output.unitTestScriptOpen = output.unitTestScriptOpen.replace(
       `import runtime from "@abaplint/runtime";`,
-      `import runtime from "../../packages/runtime/build/src/index.js";`);
+      `import * as runtime from "../../packages/runtime/build/src/index.js";`);
     fs.writeFileSync(outputFolder + path.sep + "index_open.mjs", output.unitTestScriptOpen);
     output.initializationScript = output.initializationScript.replace(
       `import runtime from "@abaplint/runtime";`,
-      `import runtime from "../../packages/runtime/build/src/index.js";`);
+      `import * as runtime from "../../packages/runtime/build/src/index.js";`);
     fs.writeFileSync(outputFolder + path.sep + "init.mjs", output.initializationScript);
 
     const buf = childProcess.execSync("node unit-test/" + name + "/index.mjs");
