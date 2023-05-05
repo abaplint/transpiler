46ed7b000b3e7163e7fc74847237a23e464b16b2
Lars Hvam <larshp@hotmail.com>
2023-05-05 15:47:36 +0200

    add testcases

---
+++ test/statements/constants.ts
@@ -0,0 +1,37 @@
+import {expect} from "chai";
+import {ABAP} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe.only("Running statements - CONSTANTS", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP();
+  });
+
+  it("simple", async () => {
+    const code = `
+    CONSTANTS cval TYPE string VALUE 'ab'.
+    WRITE cval.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("ab");
+  });
+
+  it("concat", async () => {
+    const code = `
+    CONSTANTS cval TYPE string VALUE 'a' & 'b'.
+    WRITE cval.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("ab");
+  });
+
+});
