152691559fe907870ae7b03c3beafca24fec4bbd
Lars Hvam <larshp@hotmail.com>
2023-05-25 18:55:49 +0200

    fix

--- test/statements/call.ts
+++ test/statements/call.ts
@@ -170,7 +170,7 @@ START-OF-SELECTION.
     expect(js).to.include("].lif$method");
   });
 
-  it.only("call method, simple parameter table", async () => {
+  it("call method, simple parameter table", async () => {
     const code = `
 CLASS lcl DEFINITION.
   PUBLIC SECTION.
