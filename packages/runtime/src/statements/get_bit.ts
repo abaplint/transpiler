c9d3fd0df79189e254e858cb9b5834ff8e7fb592
Lars Hvam <larshp@hotmail.com>
2023-06-02 11:49:28 +0200

    throw BIT_OFFSET_NOT_POSITIVE

--- packages/runtime/src/statements/get_bit.ts
+++ packages/runtime/src/statements/get_bit.ts
@@ -6,6 +6,9 @@ export function getBit(number: INumeric, hex: XString | Hex, output: ICharacter)
 
   const charIndex = Math.floor((number.get() - 1) / 8);
   const bitIndex = (number.get() - 1) % 8;
+  if (bitIndex < 0) {
+    throw new Error("BIT_OFFSET_NOT_POSITIVE");
+  }
 
   const h = hex.get().substr(charIndex * 2, 2);
   const parsed = parseInt(h, 16).toString(2);
