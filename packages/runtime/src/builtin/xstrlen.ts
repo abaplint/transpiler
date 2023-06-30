c7d74b2b120442be1d8971e4a6d2aac559681847
Lars Hvam <larshp@hotmail.com>
2023-06-30 09:26:34 +0200

    2.7.28: bugfix BIT-NOT (#1251)

--- packages/runtime/src/builtin/xstrlen.ts
+++ packages/runtime/src/builtin/xstrlen.ts
@@ -1,7 +1,7 @@
-import {Integer} from "../types";
+import {Hex, Integer, XString} from "../types";
 import {ICharacter} from "../types/_character";
 
-export function xstrlen(input: {val: ICharacter | string}): Integer {
+export function xstrlen(input: {val: ICharacter | string | Hex | XString}): Integer {
   if (typeof input.val === "string") {
     return new Integer().set(input.val.length / 2);
   } else {
