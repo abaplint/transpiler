898ac765f707fd1f9248824ba7b107fc1b6e3ef6
Lars Hvam <larshp@hotmail.com>
2023-08-23 07:41:29 +0200

    2.7.78: update abaplint (#1301)

--- packages/transpiler/src/statements/move_corresponding.ts
+++ packages/transpiler/src/statements/move_corresponding.ts
@@ -2,11 +2,12 @@ import * as abaplint from "@abaplint/core";
 import {IStatementTranspiler} from "./_statement_transpiler";
 import {Traversal} from "../traversal";
 import {Chunk} from "../chunk";
+import {TargetTranspiler} from "../expressions";
 
 export class MoveCorrespondingTranspiler implements IStatementTranspiler {
 
   public transpile(node: abaplint.Nodes.StatementNode, traversal: Traversal): Chunk {
-    const target = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Target));
+    const target = new TargetTranspiler().transpile(node.findDirectExpression(abaplint.Expressions.SimpleTarget)!, traversal);
     const source = traversal.traverse(node.findDirectExpression(abaplint.Expressions.Source));
 
     return new Chunk().append("abap.statements.moveCorresponding(", node, traversal)
