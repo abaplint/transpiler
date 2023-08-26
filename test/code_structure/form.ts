55144164274ffa0db1a1b68a5b5675707301f00e
Lars Hvam <larshp@hotmail.com>
2023-08-26 08:23:14 +0200

    add testcase

---
+++ test/code_structure/form.ts
@@ -0,0 +1,31 @@
+import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running code structure - FORM / PERFORM", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it("basic", async () => {
+    const code = `
+    FORM hello.
+      WRITE / 'hello'.
+    ENDFORM.
+
+    START-OF-SELECTION.
+      PERFORM hello.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("hello");
+  });
+
+});
