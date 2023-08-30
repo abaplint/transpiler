becbe1fd18a3b42ed7f3e622f81c9aad22b515e3
Lars Hvam <larshp@hotmail.com>
2023-08-30 19:17:52 +0200

    wip

--- packages/database-snowflake/src/index.ts
+++ packages/database-snowflake/src/index.ts
@@ -1,22 +1,56 @@
 import {DB} from "@abaplint/runtime";
+import * as snowflake from "snowflake-sdk";
 
 export class SnowflakeDatabaseClient implements DB.DatabaseClient {
   public readonly name = "snowflake";
+  private connection: snowflake.Connection;
 
   public constructor(_input?: {trace?: boolean}) {
     // todo
   }
 
-  public async connect(_data?: ArrayLike<number> | Buffer | null) {
-    throw "todo";
+  public async connect(input?: snowflake.ConnectionOptions): Promise<void> {
+    if (input === undefined) {
+      throw "supply connection details";
+    }
+
+    this.connection = snowflake.createConnection({
+      ...input,
+      clientSessionKeepAlive: true,
+    });
+
+    await new Promise((resolve, reject) =>
+      this.connection.connect((err, conn) => {
+        err ? reject(err) : resolve(conn);
+      })
+    );
   }
 
   public async disconnect() {
     throw "todo";
   }
 
-  public async execute(_sql: string | string[]): Promise<void> {
-    throw "todo";
+  public async execute(sql: string | string[]): Promise<void> {
+    if (typeof sql === "string") {
+      if (sql === "") {
+        return;
+      }
+      await new Promise((resolve, reject) =>
+        this.connection.execute({
+          sqlText: sql,
+          complete: function (err, _stmt, rows) {
+            err ? reject(err) : resolve(rows);
+          }}));
+    } else {
+      for (const s of sql) {
+        await new Promise((resolve, reject) =>
+          this.connection.execute({
+            sqlText: s,
+            complete: function (err, _stmt, rows) {
+              err ? reject(err) : resolve(rows);
+            }}));
+      }
+    }
   }
 
   public async beginTransaction() {
