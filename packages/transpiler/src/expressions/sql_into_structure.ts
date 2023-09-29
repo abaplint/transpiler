5a714b8afaa9094570d51f4ded29a937d8dbe75f
Lars Hvam <larshp@hotmail.com>
2023-09-29 06:49:46 +0200

    update

--- packages/transpiler/src/expressions/sql_into_structure.ts
+++ packages/transpiler/src/expressions/sql_into_structure.ts
@@ -7,17 +7,8 @@ import {Chunk} from "../chunk";
 export class SQLIntoStructureTranspiler implements IExpressionTranspiler {
 
   public transpile(node: Nodes.ExpressionNode, traversal: Traversal): Chunk {
-    const targets = node.findDirectExpressions(abaplint.Expressions.SQLTarget);
-    const transpiled = targets.map(t=> traversal.traverse(t));
-    if (targets.length === 1) {
-      return transpiled[0];
-    } else {
-      const chunk = new Chunk();
-      chunk.appendString("[");
-      chunk.appendString(transpiled.map(t => t.getCode()).join(","));
-      chunk.appendString("]");
-      return chunk;
-    }
+    const transpiled = traversal.traverse(node.findDirectExpression(abaplint.Expressions.SQLTarget));
+    return transpiled;
   }
 
 }
