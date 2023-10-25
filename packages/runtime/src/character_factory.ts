2140dc42f35799029b23bd50561e120e283b225c
Lars Hvam <larshp@hotmail.com>
2023-10-25 15:09:30 +0200

    test

--- packages/runtime/src/character_factory.ts
+++ packages/runtime/src/character_factory.ts
@@ -5,9 +5,12 @@ export class CharacterFactory {
 
   public static get(num: number, value: string): Character {
     const key = num + value;
-    if (CharacterFactory.map[key] === undefined) {
+    const lookup = CharacterFactory.map[key];
+    if (lookup === undefined) {
       CharacterFactory.map[key] = new Character(num).set(value).setConstant();
+      return CharacterFactory.map[key];
+    } else {
+      return lookup;
     }
-    return CharacterFactory.map[key];
   }
 }
