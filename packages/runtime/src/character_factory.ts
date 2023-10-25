15c2db3b143809c9b5cd8fddc8a3604595a7b174
Lars Hvam <larshp@hotmail.com>
2023-10-25 15:34:07 +0200

    test map

--- packages/runtime/src/character_factory.ts
+++ packages/runtime/src/character_factory.ts
@@ -1,6 +1,22 @@
 import {Character} from "./types";
 
+// TEST Map()
+
 export class CharacterFactory {
+  private static readonly map = new Map<string, Character>();
+
+  public static get(num: number, value: string): Character {
+    const key = num + value;
+    const lookup = CharacterFactory.map.get(key);
+    if (lookup === undefined) {
+      const char = new Character(num).set(value).setConstant();
+      CharacterFactory.map.set(key, char);
+      return char;
+    } else {
+      return lookup;
+    }
+  }
+/*
   private static readonly map: {[key: string]: Character} = {};
 
   public static get(num: number, value: string): Character {
@@ -13,4 +29,5 @@ export class CharacterFactory {
       return lookup;
     }
   }
+*/
 }
