da90ca17756e8b15149d8f9e701b7b6481ca9196
Lars Hvam <larshp@hotmail.com>
2024-01-23 09:05:50 +0100

    wip, UNPACK

---
+++ test/statements/unpack.ts
@@ -0,0 +1,28 @@
+// import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - UNPACK", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it("char to char", async () => {
+    const code = `DATA lv_char4 TYPE c LENGTH 4.
+DATA lv_char6 TYPE c LENGTH 6.
+lv_char4 = '300'.
+UNPACK lv_char4 TO lv_char6.
+ASSERT lv_char6 = '000300'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
