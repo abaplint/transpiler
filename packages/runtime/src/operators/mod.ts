f8fa3449f6d7e8126dff23a52d4baf21179f5c72
Lars Hvam <larshp@hotmail.com>
2024-01-30 16:04:08 +0100

    2.7.154: bugfix MOD + eq fixes (#1380)

--- packages/runtime/src/operators/mod.ts
+++ packages/runtime/src/operators/mod.ts
@@ -6,9 +6,16 @@ import {parse} from "./_parse";
 export function mod(left: INumeric | ICharacter | string | number, right: INumeric | ICharacter | string | number) {
   const l = parse(left);
   const r = parse(right);
+
+  let val = ( ( l % r ) + r ) % r;
+
+  if (val < 0) {
+    val = val * -1;
+  }
+
   if (left instanceof Integer8 || right instanceof Integer8) {
-    return new Integer8().set(( ( l % r ) + r ) % r);
+    return new Integer8().set(val);
   } else {
-    return new Integer().set(( ( l % r ) + r ) % r);
+    return new Integer().set(val);
   }
 }
