7332238641e7287f3ca0577173a0b1c04d8e3ac7
Lars Hvam <larshp@hotmail.com>
2024-03-03 11:36:35 +0100

    2.8.3: bugfix, hex to int8 (#1403)

--- packages/runtime/src/types/integer8.ts
+++ packages/runtime/src/types/integer8.ts
@@ -5,6 +5,8 @@ import {XString} from "./xstring";
 import {ICharacter} from "./_character";
 import {INumeric} from "./_numeric";
 import {Integer} from "./integer";
+import {getBit} from "../statements/get_bit";
+import {Character} from "./character";
 
 const digits = new RegExp(/^\s*-?\+?\d+\.?\d* *$/i);
 
@@ -39,16 +41,19 @@ export class Integer8 {
     } else if (value instanceof Float) {
       this.set(Math.round(value.getRaw()));
     } else if (value instanceof Hex || value instanceof XString) {
-// todo, can BigInt("0x" + value.get()); be used?
-      let num = parseInt(value.get(), 16);
-// handle two complement,
-      if (value instanceof Hex && value.getLength() >= 4) {
-        const maxVal = Math.pow(2, value.get().length / 2 * 8);
-        if (num > maxVal / 2 - 1) {
-          num = num - maxVal;
+      if (value.get().length === 16) {
+        const lv_bit = new Character();
+        getBit(new Integer().set(1), value, lv_bit);
+        if (lv_bit.get() === "1") {
+          const val = BigInt("0x" + value.get());
+          this.value = val - BigInt("0x10000000000000000");
+        } else {
+          this.value = BigInt("0x" + value.get());
         }
+      } else {
+// todo, what if the input is longer than 16 bytes?
+        this.value = BigInt("0x" + value.get());
       }
-      this.set(num);
     } else {
       this.set(value.get());
     }
