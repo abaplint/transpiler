34e7acfcbd2261e1a8b66e7b9a3e296fdfdc3488
Lars Hvam <larshp@hotmail.com>
2024-03-15 16:03:55 +0100

    increase

--- performance/test31.ts
+++ performance/test31.ts
@@ -1,5 +1,5 @@
 export const test31 = `
 DATA foo TYPE x LENGTH 8.
-DO 250000 TIMES.
+DO 2500000 TIMES.
   SET BIT 18 OF foo TO 1.
 ENDDO.`;
--- performance/test32.ts
+++ performance/test32.ts
@@ -1,6 +1,6 @@
 export const test32 = `
 DATA foo TYPE x LENGTH 8.
 DATA val TYPE i.
-DO 250000 TIMES.
+DO 5000000 TIMES.
   GET BIT 18 OF foo INTO val.
 ENDDO.`;
