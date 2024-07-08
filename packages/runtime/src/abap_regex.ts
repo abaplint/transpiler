0f45b7d25ff7c08ef8dd8d47976bda480e707afa
Lars Hvam <larshp@hotmail.com>
2024-07-08 12:11:27 +0200

    2.10.5: bugfix REGEX (#1451)

--- packages/runtime/src/abap_regex.ts
+++ packages/runtime/src/abap_regex.ts
@@ -9,6 +9,9 @@ export class ABAPRegExp {
     // https://github.com/micromatch/posix-character-classes#posix-character-classes
     ret = ret.replace(/\[\^\[:print:\]\]/g, "[\\x00-\\x1F\\x7F]");
 
+    // https://help.sap.com/doc/abapdocu_750_index_htm/7.50/en-US/abenregex_syntax_specials.htm
+    ret = ret.replace(/\\C/g, "[a-zA-Z]");
+
     ret = ret.replace("[[:space:]]", "\\s");
 
     return ret;
