b95449e942b5194fd1e834bf0e6f88362a50bb2b
Lars Hvam <larshp@hotmail.com>
2024-07-15 20:08:49 +0200

    wip

--- packages/transpiler/test/single_statements.ts
+++ packages/transpiler/test/single_statements.ts
@@ -274,7 +274,7 @@ await abap.Classes['KERNEL_PUSH_CHANNELS'].wait({seconds: abap.IntegerFactory.ge
     {abap: `ADD 2 to foo.`,
       js: `foo.set(abap.operators.add(foo,abap.IntegerFactory.get(2)));`},
     {abap: `ASSIGN lv_test_ref->* TO <lv_test>.`,
-      js: `abap.statements.assign({target: fs_lv_test_, source: (lv_test_ref).dereference()});`},
+      js: `abap.statements.assign({target: fs_lv_test_, source: lv_test_ref.dereference()});`},
     {abap: `LOOP AT mr_source_tree->* INTO ls_node. ENDLOOP.`,
       js: `for await (const unique1 of abap.statements.loop(mr_source_tree.dereference())) {
   ls_node.set(unique1);
