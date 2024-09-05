04c4c1d0720fe05f124764d1928032d52e072751
Lars Hvam <larshp@hotmail.com>
2024-09-05 16:18:41 +0200

    change handling of keywords

---
+++ test/keywords.ts
@@ -0,0 +1,29 @@
+import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../packages/runtime/src/";
+import {AsyncFunction, runFiles} from "./_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Keywords", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it.only("class", async () => {
+    const code = `
+DATA class TYPE x.
+class = 'AA'.
+WRITE class.`;
+
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("AA");
+  });
+
+});
