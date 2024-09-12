1c99c9a3ccd25ebaf1120a35736b9f7aeb49f8ed
Lars Hvam <larshp@hotmail.com>
2024-09-12 07:13:10 +0200

    done

---
+++ packages/runtime/src/builtin/from_mixed.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import {throwError} from "../throw_error";
+import {String} from "../types";
+import {ICharacter} from "../types/_character";
+import {INumeric} from "../types/_numeric";
+
+export function from_mixed(input: {
+  val: ICharacter | string,
+  sep?: ICharacter | string,
+  case?: ICharacter | string,
+  min?: INumeric | number }): String {
+
+  let sep = input.sep;
+  if (sep === undefined) {
+    sep = "_";
+  }
+  if (typeof sep !== "string") {
+    sep = sep.get();
+  }
+  if (sep.length === 0) {
+    throwError("CX_SY_STRG_PAR_VAL");
+  }
+
+  const min = 1;
+  if (min < 0) {
+    throwError("CX_SY_STRG_PAR_VAL");
+  }
+
+  let val = input.val;
+  if (typeof val !== "string") {
+    val = val.get();
+  }
+
+// todo: handle "case" and "min" ?
+
+  const regex = new RegExp(/([A-Z])/, "g");
+  val = val.substring(0, 1) + val.substring(1).replace(regex, "_$1");
+
+  return new String().set(val.toUpperCase());
+}
--- test/builtin/from_mixed.ts
+++ test/builtin/from_mixed.ts
@@ -14,24 +14,34 @@ describe("Builtin functions - from_mixed", () => {
     abap = new ABAP({console: new MemoryConsole()});
   });
 
-  it.only("test", async () => {
+  it("test1", async () => {
     const code = `
 WRITE / from_mixed( 'putData' ).
+`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal(`PUT_DATA`);
+  });
+
+  it("more testing", async () => {
+    const code = `
 WRITE / from_mixed( 'PutData' ).
 WRITE / from_mixed( 'PUTDATA' ).
 WRITE / from_mixed( 'putdata' ).
 WRITE / from_mixed( 'put data' ).
 WRITE / from_mixed( 'put Data' ).
+WRITE / from_mixed( '_PutData' ).
 `;
     const js = await run(code);
     const f = new AsyncFunction("abap", js);
     await f(abap);
     expect(abap.console.get()).to.equal(`PUT_DATA
-PUT_DATA
 P_U_T_D_A_T_A
 PUTDATA
 PUT DATA
-PUT _DATA`);
+PUT _DATA
+__PUT_DATA`);
   });
 
 });
