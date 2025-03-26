26bc38d83579f47c823bb789a5303a73dd550821
Lars Hvam <larshp@hotmail.com>
2025-03-26 09:28:25 +0100

    fix

--- packages/transpiler/src/unit_test.ts
+++ packages/transpiler/src/unit_test.ts
@@ -259,7 +259,7 @@ async function run() {
         // todo, some refactoring here,
         ret += `        if (test.setup) await test.setup();\n`;
         ret += `        if (test.FRIENDS_ACCESS_INSTANCE.setup) await test.FRIENDS_ACCESS_INSTANCE.setup();\n`;
-        ret += `        if (test.FRIENDS_ACCESS_INSTANCE.SUPER.setup) await test.FRIENDS_ACCESS_INSTANCE.SUPER.setup();\n`;
+        ret += `        if (test.FRIENDS_ACCESS_INSTANCE.SUPER && test.FRIENDS_ACCESS_INSTANCE.SUPER.setup) await test.FRIENDS_ACCESS_INSTANCE.SUPER.setup();\n`;
         ret += `        console.log("${st.obj.getName()}: running ${lc}->${m}");\n`;
         ret += `        meth = locl.addMethod("${m}");\n`;
         ret += `        await test.FRIENDS_ACCESS_INSTANCE.${m}();\n`;
