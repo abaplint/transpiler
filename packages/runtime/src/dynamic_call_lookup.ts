b8c8c047ae731cd26cf714ba1259c95d5bd5c8e8
Lars Hvam <larshp@hotmail.com>
2025-03-26 12:22:10 +0100

    2.10.31: bugfix dynamic call (#1493)

---
+++ packages/runtime/src/dynamic_call_lookup.ts
@@ -0,0 +1,22 @@
+import {ICharacter} from "./types/_character";
+
+export function dynamicCallLookup(obj: any, methodName: ICharacter): any {
+  // todo: escape interface and namespace characters in name?
+  const name = methodName.get().toLowerCase().trimEnd();
+
+  let ret = obj[name];
+
+  if (ret === undefined) {
+    // it might be private, note this currently does not respect encapsulation properly
+    ret = obj.FRIENDS_ACCESS_INSTANCE[name];
+  }
+
+  if (ret === undefined) {
+    // @ts-ignore
+    if (abap.Classes['CX_SY_DYN_CALL_ILLEGAL_METHOD'] === undefined) { throw "CX_SY_DYN_CALL_ILLEGAL_METHOD not found"; }
+    // @ts-ignore
+    throw new abap.Classes['CX_SY_DYN_CALL_ILLEGAL_METHOD']();
+  }
+
+  return ret;
+}
