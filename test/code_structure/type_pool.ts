e229fa0f560141d8d3e40a38093bc95223be5b29
Lars Hvam <larshp@hotmail.com>
2025-04-03 08:19:08 +0200

    bugfix structured constant

--- test/code_structure/type_pool.ts
+++ test/code_structure/type_pool.ts
@@ -19,4 +19,21 @@ pool['seoo_sdf'] = new abap.types.Character(1, {"qualifiedName":"seoo_sdf"});
 abap.TypePools['SEOO'] = pool;`);
   });
 
+  it.only("structured constant", async () => {
+    const code = `TYPE-POOL seoo.
+CONSTANTS: BEGIN OF seoo_moo,
+             foo TYPE n LENGTH 1 VALUE '5',
+           END OF seoo_moo.`;
+    const output = await compileFiles([
+      {filename: "seoo.type.abap", contents: code},
+    ]);
+    expect(output.objects.length).to.equal(1);
+
+    expect(output.objects[0].chunk.getCode()).to.equal(`const pool = {};
+pool['seoo_cmptype_attribute'] = new abap.types.Numc();
+pool['seoo_cmptype_attribute'].set('5');
+pool['seoo_sdf'] = new abap.types.Character(1, {"qualifiedName":"seoo_sdf"});
+abap.TypePools['SEOO'] = pool;`);
+  });
+
 });
