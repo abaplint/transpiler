e0bff568065cdb27ae63db6c54829a8b945efcb6
Lars HVam <larshp@hotmail.com>
2025-04-08 17:21:54 +0200

    extract logic

--- packages/transpiler/src/unit_test.ts
+++ packages/transpiler/src/unit_test.ts
@@ -1,62 +1,10 @@
 /* eslint-disable max-len */
 import * as abaplint from "@abaplint/core";
-import {DatabaseSetupResult} from "./db/database_setup_result";
+import {escapeNamespaceFilename} from "./initialization";
 
 export type TestMethodList = {object: string, class: string, method: string}[];
 
 export class UnitTest {
-
-  // todo, move this method somewhere else, its much more than just unit test relevant
-  public initializationScript(reg: abaplint.IRegistry, dbSetup: DatabaseSetupResult, extraSetup?: string, useImport?: boolean) {
-    let ret = "";
-    if (useImport === true) {
-      ret = `/* eslint-disable import/newline-after-import */
-import "./_top.mjs";\n`;
-    } else {
-      ret = `/* eslint-disable import/newline-after-import */
-import runtime from "@abaplint/runtime";
-globalThis.abap = new runtime.ABAP();\n`;
-    }
-
-    ret += `${this.buildImports(reg, useImport)}
-
-export async function initializeABAP() {\n`;
-    ret += `  const sqlite = [];\n`;
-    for (const i of dbSetup.schemas.sqlite) {
-      ret += `  sqlite.push(\`${i}\`);\n`;
-    }
-    ret += `  const hdb = \`${dbSetup.schemas.hdb}\`;\n`;
-    ret += `  const pg = [];\n`;
-    for (const i of dbSetup.schemas.pg) {
-      ret += `  pg.push(\`${i}\`);\n`;
-    }
-    ret += `  const snowflake = [];\n`;
-    for (const i of dbSetup.schemas.snowflake) {
-      ret += `  snowflake.push(\`${i}\`);\n`;
-    }
-    ret += `  const schemas = {sqlite, hdb, pg, snowflake};\n`;
-    ret += `\n`;
-
-    ret += `  const insert = [];\n`;
-    for (const i of dbSetup.insert) {
-      ret += `  insert.push(\`${i}\`);\n`;
-    }
-    ret += `\n`;
-
-    if (extraSetup === undefined || extraSetup === "") {
-      ret += `// no setup logic specified in config\n`;
-    } else {
-      ret += `  const {setup} = await import("${extraSetup}");\n` +
-             `  await setup(globalThis.abap, schemas, insert);\n`;
-    }
-    ret += `}`;
-    return ret;
-  }
-
-  private escapeNamespace(filename: string): string {
-// ES modules are resolved and cached as URLs. This means that special characters must be percent-encoded, such as # with %23 and ? with %3F.
-    return filename.replace(/\//g, "%23");
-  }
 
   public unitTestScriptOpen(reg: abaplint.IRegistry, _skip?: TestMethodList): string {
     let ret = `/* eslint-disable curly */
@@ -80,7 +28,7 @@ async function run() {
       }
       const hasTestFile = obj.getFiles().some(f => { return f.getFilename().includes(".testclasses."); });
       if (hasTestFile === true) {
-        ret += `  await import("./${this.escapeNamespace(obj.getName().toLowerCase())}.${obj.getType().toLowerCase()}.testclasses.mjs");\n`;
+        ret += `  await import("./${escapeNamespaceFilename(obj.getName().toLowerCase())}.${obj.getType().toLowerCase()}.testclasses.mjs");\n`;
       }
 
       for (const file of obj.getABAPFiles()) {
@@ -230,7 +178,7 @@ async function run() {
       ret += `// --------------------------------------------\n`;
       const lc = st.localClass.toLowerCase();
       ret += `    {
-        const {${lc}} = await import("./${this.escapeNamespace(st.obj.getName().toLowerCase())}.${st.obj.getType().toLowerCase()}.testclasses.mjs");
+        const {${lc}} = await import("./${escapeNamespaceFilename(st.obj.getName().toLowerCase())}.${st.obj.getType().toLowerCase()}.testclasses.mjs");
         if (${lc}.class_setup) await ${lc}.class_setup();\n`;
 
       for (const m of st.methods) {
@@ -271,72 +219,6 @@ run().then(() => {
 });`;
 
     return ret;
-  }
-
-  private buildImports(reg: abaplint.IRegistry, useImport?: boolean): string {
-// note: ES modules are hoised, so use the dynamic import(), due to setting of globalThis.abap
-// some sorting required: eg. a class constructor using constant from interface
-
-    const list: string[] = [];
-    const late: string[] = [];
-
-    const imp = (filename: string) => {
-      if (useImport === true) {
-        return `import "./${filename}.mjs";`;
-      } else {
-        return `await import("./${filename}.mjs");`;
-      }
-    };
-
-    for (const obj of reg.getObjects()) {
-      if (obj instanceof abaplint.Objects.Table
-          || obj instanceof abaplint.Objects.DataElement
-          || obj instanceof abaplint.Objects.LockObject
-          || obj instanceof abaplint.Objects.MessageClass
-          || obj instanceof abaplint.Objects.MIMEObject
-          || obj instanceof abaplint.Objects.Oauth2Profile
-          || obj instanceof abaplint.Objects.WebMIME
-          || obj instanceof abaplint.Objects.TypePool
-          || obj instanceof abaplint.Objects.TableType) {
-        list.push(imp(`${this.escapeNamespace(obj.getName().toLowerCase())}.${obj.getType().toLowerCase()}`));
-      }
-    }
-
-    for (const obj of reg.getObjects()) {
-      if (obj instanceof abaplint.Objects.FunctionGroup) {
-        list.push(imp(`${this.escapeNamespace(obj.getName().toLowerCase())}.fugr`));
-      } else if (obj instanceof abaplint.Objects.Class) {
-        if (obj.getName().toUpperCase() !== "CL_ABAP_CHAR_UTILITIES"
-            && this.hasClassConstructor(reg, obj)) {
-          // this will not solve all problems with class constors 100%, but probably good enough
-          late.push(imp(`${this.escapeNamespace(obj.getName().toLowerCase())}.${obj.getType().toLowerCase()}`));
-        } else {
-          list.push(imp(`${this.escapeNamespace(obj.getName().toLowerCase())}.${obj.getType().toLowerCase()}`));
-        }
-      } else if (obj instanceof abaplint.Objects.Interface) {
-        list.push(imp(`${this.escapeNamespace(obj.getName().toLowerCase())}.${obj.getType().toLowerCase()}`));
-      }
-    }
-
-    return [...list.sort(), ...late].join("\n");
-  }
-
-  // class constructors might make early use of eg. constants from interfaces
-  // sub classes will import() super classes and trigger a class constructor of the super
-  private hasClassConstructor(reg: abaplint.IRegistry, clas: abaplint.Objects.Class): boolean {
-    if (clas.getDefinition()?.getMethodDefinitions().getByName("CLASS_CONSTRUCTOR") !== undefined) {
-      return true;
-    }
-
-    const sup = clas.getDefinition()?.getSuperClass();
-    if (sup !== undefined) {
-      const superClass = reg.getObject("CLAS", sup) as abaplint.Objects.Class | undefined;
-      if (superClass) {
-        return this.hasClassConstructor(reg, superClass);
-      }
-    }
-
-    return false;
   }
 
 }
