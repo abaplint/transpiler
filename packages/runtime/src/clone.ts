a981bbeb7017c93c1d01bb98366c0ff68c2efe02
Lars Hvam <larshp@hotmail.com>
2025-04-27 12:23:32 +0200

    cleanup

--- packages/runtime/src/clone.ts
+++ packages/runtime/src/clone.ts
@@ -1,21 +1,13 @@
-import {ABAPObject, DataReference, HexUInt8} from "./types";
+import {DataReference} from "./types";
 
 export function clone<T>(obj: T): T {
   // @ts-ignore
   if (obj.clone) {
     // @ts-ignore
     return obj.clone() as T;
-  } else if (obj instanceof ABAPObject) {
-    const n = new ABAPObject();
-    n.set(obj.get());
-    return n as T;
   } else if (obj instanceof DataReference) {
     const n = new DataReference(obj.getType());
     n.assign(obj.getPointer());
-    return n as T;
-  } else if (obj instanceof HexUInt8) {
-    const n = new HexUInt8({length: obj.getLength(), qualifiedName: obj.getQualifiedName()});
-    n.set(obj.get());
     return n as T;
   }
 
