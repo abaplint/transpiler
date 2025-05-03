e76e8ca2af1dfb81748afdfab1dc375c881282c2
Lars Hvam <larshp@hotmail.com>
2025-05-03 14:33:02 +0200

    condense

--- packages/runtime/src/statements/condense.ts
+++ packages/runtime/src/statements/condense.ts
@@ -1,12 +1,17 @@
 import {ICharacter} from "../types/_character";
 
+const ENDS_WITH_SPACE = / +$/;
+const BEGINS_WITH_SPACE = /^ +/;
+const ANY_SPACES = / */g;
+const MULTIPLE_SPACES_REGEX = / {2,}/g;
+
 export function condense(input: ICharacter, options: {nogaps: boolean}): void {
-  let trimmed = input.get().replace(/ +$/, "");
-  trimmed = trimmed.replace(/^ +/, "");
+  let trimmed = input.get().replace(ENDS_WITH_SPACE, "");
+  trimmed = trimmed.replace(BEGINS_WITH_SPACE, "");
   if (options.nogaps) {
-    trimmed = trimmed.replace(/ */g,"");
+    trimmed = trimmed.replace(ANY_SPACES, "");
   } else {
-    trimmed = trimmed.replace(/ {2,}/g," ");
+    trimmed = trimmed.replace(MULTIPLE_SPACES_REGEX, " ");
   }
   input.set(trimmed);
 }
