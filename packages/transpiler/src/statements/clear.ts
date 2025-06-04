244de24e51575ddc35f677f7c758fd1352820679
Lars Hvam <larshp@hotmail.com>
2025-06-04 17:34:36 +0200

    wip

--- packages/transpiler/src/statements/clear.ts
+++ packages/transpiler/src/statements/clear.ts
@@ -10,10 +10,12 @@ export class ClearTranspiler implements IStatementTranspiler {
 
     const ret = new Chunk();
 
+    /*
     ret.appendChunk(target);
     ret.append(".clear();", node.getLastToken().getEnd(), traversal);
+    */
 
-//    ret.append(target.getCode() + ".clear();", node, traversal);
+    ret.append(target.getCode() + ".clear();", node, traversal);
 
 /*
     ret.append(target.getCode(), node.getFirstToken().getStart(), traversal);
