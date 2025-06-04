101b8126522cd6a3156b552293fd67028eb37575
Lars Hvam <larshp@hotmail.com>
2025-06-04 14:16:19 +0200

    wip

--- web/transpiler/src/index.ts
+++ web/transpiler/src/index.ts
@@ -21,6 +21,7 @@ import {ABAP, MemoryConsole} from "@abaplint/runtime";
 import * as abaplint from "@abaplint/core";
 import * as abapMonaco from "@abaplint/monaco";
 import Split from "split-grid";
+import {registerDummyHoverProvider} from '../hover';
 
 const reg = new abaplint.Registry(new abaplint.Config(JSON.stringify(config)));
 abapMonaco.registerABAP(reg);
@@ -56,7 +57,10 @@ const editor1 = monaco.editor.create(document.getElementById("container1"), {
 editor1.addCommand(
 	monaco.KeyCode.F9,
   () => {
-		console.log("my command is executing!");
+    // Register dummy hover provider for both editors' languages
+    registerDummyHoverProvider(editor1.getModel().getLanguageId());
+    registerDummyHoverProvider(editor2.getModel().getLanguageId());
+    console.log('Hover enabled');
 	},
 );
 
