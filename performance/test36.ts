f0bf83978edacc88ff228eb2f4dfe214ed4f9a9b
Lars Hvam <larshp@hotmail.com>
2025-06-26 09:22:49 +0200

    add perf tests

---
+++ performance/test36.ts
@@ -0,0 +1,7 @@
+export const test35 = `
+DATA char TYPE c LENGTH 10.
+
+DO 10000000 TIMES.
+  CLEAR char.
+ENDDO.
+`;
---
+++ performance/test37.ts
@@ -0,0 +1,7 @@
+export const test35 = `
+DATA numc TYPE n LENGTH 10.
+
+DO 10000000 TIMES.
+  CLEAR numc.
+ENDDO.
+`;
