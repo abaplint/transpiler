5d806ecce3eeda1463814f1e4a53d636db891e1a
Lars Hvam <5888506+larshp@users.noreply.github.com>
2025-07-29 15:18:45 +0200

    2.10.76: bugfix, MOVE-CORRESPONDING (#1545)

--- packages/runtime/src/statements/move_corresponding.ts
+++ packages/runtime/src/statements/move_corresponding.ts
@@ -1,7 +1,26 @@
-import {Structure} from "../types";
+import {FieldSymbol, Structure} from "../types";
 
-export function moveCorresponding(source: Structure, target: Structure): void {
+export function moveCorresponding(source: Structure | FieldSymbol, target: Structure | FieldSymbol): void {
+  if (source instanceof FieldSymbol) {
+    if (source.isAssigned() === false) {
+      throw new Error("GETWA_NOT_ASSIGNED");
+    }
+    moveCorresponding(source.getPointer(), target);
+    return;
+  }
+  if (target instanceof FieldSymbol) {
+    if (target.isAssigned() === false) {
+      throw new Error("GETWA_NOT_ASSIGNED");
+    }
+    moveCorresponding(source, target.getPointer());
+    return;
+  }
+
   for (const n in source.get()) {
-    target.get()[n]?.set(source.get()[n]);
+    if (target.get()[n] instanceof Structure) {
+      moveCorresponding(source.get()[n], target.get()[n]);
+    } else {
+      target.get()[n]?.set(source.get()[n]);
+    }
   }
 }
--- test/statements/move_corresponding.ts
+++ test/statements/move_corresponding.ts
@@ -30,4 +30,61 @@ WRITE foo-field.`;
     expect(abap.console.get()).to.equal("2");
   });
 
+  it("sub structures with more fields", async () => {
+    const code = `
+TYPES: BEGIN OF ty1,
+         BEGIN OF sub,
+           field TYPE i,
+           more  TYPE i,
+         END OF sub,
+       END OF ty1.
+
+TYPES: BEGIN OF ty2,
+         BEGIN OF sub,
+           field TYPE i,
+         END OF sub,
+       END OF ty2.
+
+DATA data1 TYPE ty1.
+DATA data2 TYPE ty2.
+
+data1-sub-field = 2.
+MOVE-CORRESPONDING data1 TO data2.
+WRITE / data2-sub-field.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("2");
+  });
+
+  it("FS target", async () => {
+    const code = `
+TYPES: BEGIN OF ty,
+         field TYPE i,
+       END OF ty.
+DATA val1 TYPE ty.
+DATA val2 TYPE ty.
+FIELD-SYMBOLS <fs> TYPE any.
+ASSIGN val2 TO <fs>.
+MOVE-CORRESPONDING val1 TO <fs>.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+  it("FS source", async () => {
+    const code = `
+TYPES: BEGIN OF ty,
+         field TYPE i,
+       END OF ty.
+DATA val1 TYPE ty.
+DATA val2 TYPE ty.
+FIELD-SYMBOLS <fs> TYPE any.
+ASSIGN val2 TO <fs>.
+MOVE-CORRESPONDING <fs> TO val1.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
 });
