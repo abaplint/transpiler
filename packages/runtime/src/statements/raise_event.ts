4b7213a6bf70d4f97990466dacdd5cedbf07fa09
Lars Hvam <larshp@hotmail.com>
2025-08-02 14:03:00 +0200

    wip

--- packages/runtime/src/statements/raise_event.ts
+++ packages/runtime/src/statements/raise_event.ts
@@ -1,3 +1,3 @@
-export function raiseEvent(eventName: string) {
+export function raiseEvent(eventName: string, _parameters?: object) {
   console.log("Transpiler, RAISE EVENT/emit: " + eventName);
 }
