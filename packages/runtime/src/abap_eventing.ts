48ba54ddea92816ca21495ee879eee546c41a07f
Lars Hvam <larshp@hotmail.com>
2025-08-03 15:00:45 +0200

    wip

--- packages/runtime/src/abap_eventing.ts
+++ packages/runtime/src/abap_eventing.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import {ABAPObject} from "./types";
 
 export type ABAPEventReference = {
@@ -6,26 +5,38 @@ export type ABAPEventReference = {
   EVENT_CLASS: string;
 };
 
+type HandlerMethod = (parameters?: any) => Promise<void>;
+
 type Handlers = {
-  handlers: Function[];
-  forObject: ABAPObject | "ALL";
+  handlers: HandlerMethod[];
+  forObject: WeakRef<ABAPObject> | "ALL";
 }[];
 
 export class ABAPEventing {
   private readonly registrations: {[className: string]: {[eventName: string]: Handlers}} = {};
 
-  /**
-   * activation: true to activate the event, false to deactivate
-   */
-  public setHandler(event: ABAPEventReference, methods: Function[], forObject: ABAPObject | "ALL", activation: boolean): any {
+  public setHandler(event: ABAPEventReference, methods: HandlerMethod[], forObject: ABAPObject | "ALL", activation: boolean): any {
     if (activation === false) {
+      // todo
       throw new Error("ABAPEventing.setHandler: deactivation not supported");
     } else if (methods.length === 0) {
       throw new Error("ABAPEventing.setHandler: no methods provided");
     }
 
+    if (!this.registrations[event.EVENT_CLASS]) {
+      this.registrations[event.EVENT_CLASS] = {};
+    }
+    if (!this.registrations[event.EVENT_CLASS][event.EVENT_NAME]) {
+      this.registrations[event.EVENT_CLASS][event.EVENT_NAME] = [];
+    }
 
-    const ref = new WeakRef(forObject);
+    const ref = forObject === "ALL" ? "ALL" : new WeakRef(forObject);
+    const handlers = this.registrations[event.EVENT_CLASS][event.EVENT_NAME];
+    // todo: tackle duplicates
+    handlers.push({
+      handlers: methods,
+      forObject: ref,
+    });
   }
 
   public raiseEvent(event: ABAPEventReference, parameters?: object): any {
