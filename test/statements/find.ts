827984e0029447ee89e6431735a7a87a44561195
Lars Hvam <larshp@hotmail.com>
2025-08-11 16:57:24 +0200

    cleanup

--- test/statements/find.ts
+++ test/statements/find.ts
@@ -753,7 +753,6 @@ ENDFORM.
 START-OF-SELECTION.
   PERFORM foo.`;
     const js = await run(code);
-    console.dir(js);
     const f = new AsyncFunction("abap", js);
     await f(abap);
     expect(abap.console.get()).to.equal("0");
