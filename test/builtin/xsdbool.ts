f9d710a7facef4efc17fc3697c424fffbc1465fa
Lars Hvam <larshp@hotmail.com>
2025-08-13 13:33:19 +0200

    xsdbool

---
+++ test/builtin/xsdbool.ts
@@ -0,0 +1,31 @@
+// import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe.only("Builtin functions - xsdbool", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it("xsdbool true", async () => {
+    const code = `ASSERT xsdbool( 1 = 1 ) = abap_true.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+  it("xsdbool false", async () => {
+    const code = `ASSERT xsdbool( 1 = 2 ) = abap_false.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
+});
