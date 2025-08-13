74e4f081204387c6c7bd278e33c2b408e291637f
Lars Hvam <larshp@hotmail.com>
2025-08-13 14:15:02 +0200

    wip

--- packages/transpiler/src/expressions/new_object.ts
+++ packages/transpiler/src/expressions/new_object.ts
@@ -1,12 +1,21 @@
-import {Nodes} from "@abaplint/core";
+import {Nodes, Expressions} from "@abaplint/core";
 import {IExpressionTranspiler} from "./_expression_transpiler";
 import {Traversal} from "../traversal";
 import {Chunk} from "../chunk";
+import {TypeNameOrInfer} from "./type_name_or_infer";
 
 export class NewObjectTranspiler implements IExpressionTranspiler {
 
-  public transpile(_node: Nodes.ExpressionNode, _traversal: Traversal): Chunk {
+  public transpile(node: Nodes.ExpressionNode, traversal: Traversal): Chunk {
     const ret = new Chunk();
+
+    const typeNameOrInfer = node.findDirectExpression(Expressions.TypeNameOrInfer);
+    if (typeNameOrInfer === undefined) {
+      throw new Error("NewObjectTranspiler: TypeNameOrInfer not found");
+    }
+
+    const type = new TypeNameOrInfer().findType(typeNameOrInfer, traversal);
+    console.dir(type);
 
     ret.appendString("SDFSDFSDF");
 
