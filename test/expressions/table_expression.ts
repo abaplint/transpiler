100b146817aeb1fcfffce3e8233305b7c9694b41
Lars Hvam <larshp@hotmail.com>
2025-08-14 16:26:45 +0200

    add

--- test/expressions/table_expression.ts
+++ test/expressions/table_expression.ts
@@ -60,4 +60,56 @@ WRITE / int[ table_line->name = 'hello' ]->bar.`;
     expect(abap.console.get()).to.equal("2");
   });
 
+  it("calculated index", async () => {
+    const code = `
+DATA tab TYPE STANDARD TABLE OF i WITH DEFAULT KEY.
+INSERT 2 INTO TABLE tab.
+WRITE / tab[ 0 + 1 ].`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("2");
+  });
+
+  it("table line", async () => {
+    const code = `
+DATA tab TYPE STANDARD TABLE OF i WITH DEFAULT KEY.
+INSERT 2 INTO TABLE tab.
+WRITE / tab[ table_line = 2 ].`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("2");
+  });
+
+  it("multiple conditions", async () => {
+    const code = `
+TYPES: BEGIN OF ty,
+         name TYPE string,
+         bar  TYPE i,
+       END OF ty.
+DATA int TYPE STANDARD TABLE OF ty WITH DEFAULT KEY.
+INSERT VALUE #( name = 'hello' bar = 2 ) INTO TABLE int.
+WRITE / int[ name = 'hello' bar = 2 ]-bar.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("2");
+  });
+
+  it("throw exception", async () => {
+    const code = `
+DATA tab TYPE STANDARD TABLE OF i WITH DEFAULT KEY.
+WRITE / tab[ 1 ].`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+
+    try {
+      await f(abap);
+      expect.fail();
+    } catch (e) {
+      expect(e.toString()).to.contain("CX_SY_ITAB_LINE_NOT_FOUND");
+    }
+  });
+
 });
