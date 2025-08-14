76922a33f6391554302c0c041eb0f859d5018b67
Lars Hvam <larshp@hotmail.com>
2025-08-14 19:30:45 +0200

    foo

--- packages/runtime/src/builtin/line_exists.ts
+++ packages/runtime/src/builtin/line_exists.ts
@@ -1,5 +1,12 @@
 import {ICharacter} from "../types/_character";
 
 export function line_exists(callback: () => void): ICharacter {
+
+  try {
+    callback();
+  } catch (error) {
+    throw new Error("runtime line_exists() todo");
+  }
+
   throw new Error("runtime line_exists() todo");
 }
--- packages/runtime/src/builtin/line_index.ts
+++ packages/runtime/src/builtin/line_index.ts
@@ -1,5 +1,12 @@
 import {Integer} from "../types";
 
 export function line_index(callback: () => void): Integer {
+
+  try {
+    callback();
+  } catch (error) {
+    throw new Error("runtime line_index() todo");
+  }
+
   throw new Error("runtime line_index() todo");
 }
