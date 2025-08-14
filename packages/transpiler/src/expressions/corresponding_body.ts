5bce55d04f0e2b73a57854ab1f2017ca198ca0f7
Lars Hvam <larshp@hotmail.com>
2025-08-14 15:00:39 +0200

    update

--- packages/transpiler/src/expressions/corresponding_body.ts
+++ packages/transpiler/src/expressions/corresponding_body.ts
@@ -11,17 +11,24 @@ export class CorrespondingBodyTranspiler {
     if (!(typ.get() instanceof Expressions.TypeNameOrInfer)) {
       throw new Error("CorrespondingBodyTranspiler, Expected TypeNameOrInfer");
     }
+    const type = new TypeNameOrInfer().findType(typ, traversal);
+    let target = TranspileTypes.toType(type);
+    let source: Chunk | undefined;
 
-    if (body.getChildren().length !== 1) {
-      throw new Error("CorrespondingBodyTranspiler, Expected single child, todo");
+    for (const child of body.getChildren()) {
+      const c = child.get();
+      if (c instanceof Expressions.Source && child instanceof Nodes.ExpressionNode) {
+        source = traversal.traverse(child);
+      } else if (c instanceof Expressions.CorrespondingBodyBase && child instanceof Nodes.ExpressionNode) {
+        source = traversal.traverse(child?.findDirectExpression(Expressions.Source));
+        target = `abap.statements.moveCorresponding(${source!.getCode()}, ${target})`;
+      } else {
+        throw new Error("CorrespondingBodyTranspiler, todo, " + c.constructor.name);
+      }
     }
 
-    const source = traversal.traverse(body.getChildren()[0]);
-    const type = new TypeNameOrInfer().findType(typ, traversal);
-    const target = TranspileTypes.toType(type);
-
     const ret = new Chunk();
-    ret.appendString(`abap.statements.moveCorresponding(${source.getCode()}, ${target})`);
+    ret.appendString(`abap.statements.moveCorresponding(${source!.getCode()}, ${target})`);
     return ret;
   }
 
