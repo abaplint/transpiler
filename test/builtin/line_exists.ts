51f16e9f8ec49fcc712acaa9e72a18d92152e91e
Lars Hvam <larshp@hotmail.com>
2025-08-14 19:33:03 +0200

    add tests

---
+++ test/builtin/line_exists.ts
@@ -0,0 +1,47 @@
+import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Builtin functions - line_exists", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it("positive", async () => {
+    const code = `
+DATA tab TYPE STANDARD TABLE OF i WITH EMPTY KEY.
+INSERT VALUE #( ) INTO TABLE tab.
+IF line_exists( tab[ 1 ] ).
+  WRITE / 'yes'.
+ELSE.
+  WRITE / 'no'.
+ENDIF.
+      `;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal(`yes`);
+  });
+
+  it("negative", async () => {
+    const code = `
+DATA tab TYPE STANDARD TABLE OF i WITH EMPTY KEY.
+IF line_exists( tab[ 1 ] ).
+  WRITE / 'yes'.
+ELSE.
+  WRITE / 'no'.
+ENDIF.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal(`no`);
+  });
+
+});
