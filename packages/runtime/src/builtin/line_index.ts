8b96b3ef6bed9382e9c130ec554e35b7728df00a
Lars Hvam <larshp@hotmail.com>
2025-08-15 08:18:38 +0200

    wip

--- packages/runtime/src/builtin/line_index.ts
+++ packages/runtime/src/builtin/line_index.ts
@@ -1,5 +1,6 @@
 import {ABAP} from "..";
 import {Integer} from "../types";
+import {LINE_NOT_FOUND} from "./line_exists";
 
 declare const abap: ABAP;
 
@@ -8,7 +9,13 @@ export function line_index(callback: () => void): Integer {
   try {
     callback();
   } catch (error) {
-    throw new Error("runtime line_index() todo");
+    if (abap.Classes[LINE_NOT_FOUND] !== undefined
+        && error instanceof abap.Classes[LINE_NOT_FOUND]) {
+      return abap.IntegerFactory.get(-1);
+    } else if (error.toString() === `Error: Global class ${LINE_NOT_FOUND} not found`) {
+      return abap.IntegerFactory.get(-1);
+    }
+    throw error;
   }
 
   throw new Error("runtime line_index() todo");
