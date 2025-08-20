9fc4c7b5dfebf0bd5ebcddc09d78fa98c89008fa
Lars Hvam <5888506+larshp@users.noreply.github.com>
2025-08-20 14:45:21 +0200

    2.11.36: SELECT (#1585)

--- test/database.ts
+++ test/database.ts
@@ -1641,10 +1641,10 @@ ASSERT result-arbgb IS NOT INITIAL.`;
       {filename: "zag_unit_test.msag.xml", contents: msag_zag_unit_test}];
     await runAllDatabases(abap, files, () => {
       expect(abap.console.get().trimEnd()).to.equal("");
-    }, {skipVersionCheck: true});
+    });
   });
 
-  it.skip("into inline definition", async () => {
+  it("into inline definition", async () => {
     const code = `
 FORM foo.
   SELECT SINGLE * FROM t100 INTO @DATA(result).
@@ -1659,10 +1659,10 @@ START-OF-SELECTION.
       {filename: "zag_unit_test.msag.xml", contents: msag_zag_unit_test}];
     await runAllDatabases(abap, files, () => {
       expect(abap.console.get().trimEnd()).to.equal("");
-    }, {skipVersionCheck: true});
+    });
   });
 
-  it.skip("into inline definition, single field", async () => {
+  it("into inline definition, single field", async () => {
     const code = `
 FORM foo.
   SELECT SINGLE arbgb FROM t100 INTO @DATA(result).
@@ -1677,7 +1677,7 @@ START-OF-SELECTION.
       {filename: "zag_unit_test.msag.xml", contents: msag_zag_unit_test}];
     await runAllDatabases(abap, files, () => {
       expect(abap.console.get().trimEnd()).to.equal("");
-    }, {skipVersionCheck: true});
+    });
   });
 
 // todo: comma list
