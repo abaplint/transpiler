89f7e7fd8c8725b7fdf4a3436892cc34a0d0b225
Lars Hvam <5888506+larshp@users.noreply.github.com>
2025-08-22 08:28:40 +0200

    2.11.44: bugfix SQL (#1593)

--- packages/runtime/src/expand_dynamic.ts
+++ packages/runtime/src/expand_dynamic.ts
@@ -10,7 +10,7 @@ export function expandDynamic(code: string, evaluate: (name: string) => FieldSym
     code = code.replace(/ EQ /g, " = ");
 
 // todo more here, this is just one simple case,
-    let regex = /(\w+) IN <(\w+)>-(\w+)/;
+    let regex = /(\w+) IN @?<(\w+)>-(\w+)/;
     while(true) {
       const match = code.match(regex);
       if (match && match[1] && match[2] && match[3]) {
@@ -30,7 +30,7 @@ export function expandDynamic(code: string, evaluate: (name: string) => FieldSym
     }
 
 // todo more here, this is just one simple case,
-    regex = / <(\w+)>-(\w+)/;
+    regex = / @?<(\w+)>-(\w+)/;
     while(true) {
       const match = code.match(regex);
       if (match && match[1] && match[2]) {
@@ -50,7 +50,7 @@ export function expandDynamic(code: string, evaluate: (name: string) => FieldSym
     }
 
 // todo more here, this is just one simple case,
-    regex = / <(\w+)>/;
+    regex = / @?<(\w+)>/;
     while(true) {
       const match = code.match(regex);
       if (match && match[1]) {
--- test/database.ts
+++ test/database.ts
@@ -1768,4 +1768,24 @@ START-OF-SELECTION.
     });
   });
 
+  it("dynamic WHERE ATtted", async () => {
+    const code = `
+DATA ls_row TYPE t100.
+DATA lv_where TYPE string.
+DATA value TYPE t100.
+FIELD-SYMBOLS <row> TYPE t100.
+ASSIGN value TO <row>.
+<row>-arbgb = 'ZAG_UNIT_TEST'.
+lv_where = |arbgb = @<row>-arbgb|.
+SELECT SINGLE * FROM t100 INTO @ls_row WHERE (lv_where).
+WRITE / sy-subrc.`;
+    const files = [
+      {filename: "zfoobar_database.prog.abap", contents: code},
+      {filename: "t100.tabl.xml", contents: tabl_t100xml},
+      {filename: "zag_unit_test.msag.xml", contents: msag_zag_unit_test}];
+    await runAllDatabases(abap, files, () => {
+      expect(abap.console.get().trimEnd()).to.equal("0");
+    });
+  });
+
 });
