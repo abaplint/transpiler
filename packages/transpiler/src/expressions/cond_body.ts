463a1229294fbf3034f1b307d742a850c386551e
Lars Hvam <5888506+larshp@users.noreply.github.com>
2025-08-22 16:39:00 +0200

    2.11.46: COND (#1595)

---
+++ packages/transpiler/src/expressions/cond_body.ts
@@ -0,0 +1,76 @@
+import {Expressions, Nodes} from "@abaplint/core";
+import {Traversal} from "../traversal";
+import {Chunk} from "../chunk";
+import {TypeNameOrInfer} from "./type_name_or_infer";
+import {TranspileTypes} from "../transpile_types";
+import {CondTranspiler} from "./cond";
+import {SourceTranspiler} from "./source";
+
+export class CondBodyTranspiler {
+
+  public transpile(typ: Nodes.ExpressionNode, body: Nodes.ExpressionNode, traversal: Traversal): Chunk {
+
+    if (!(typ.get() instanceof Expressions.TypeNameOrInfer)) {
+      throw new Error("CondBodyTranspiler, Expected TypeNameOrInfer");
+    } else if (body.findDirectExpression(Expressions.Let)) {
+      throw new Error("CondBodyTranspiler, Let not supported, todo");
+    }
+
+    const whenThen: {when: Nodes.ExpressionNode, then: Nodes.ExpressionNode}[] = [];
+    const expressions: Nodes.ExpressionNode[] = [];
+
+    for (const c of body.getChildren()) {
+      if (c instanceof Nodes.TokenNode) {
+        if (c.concatTokens() === "ELSE") {
+          break;
+        }
+      } else {
+        expressions.push(c);
+      }
+    }
+
+    for (let i = 0; i < expressions.length; i = i + 2) {
+      whenThen.push({when: expressions[i], then: expressions[i + 1]});
+    }
+
+    const type = new TypeNameOrInfer().findType(typ, traversal);
+    const target = TranspileTypes.toType(type);
+
+    const ret = new Chunk();
+    ret.appendString("(" + target + ".set(");
+    ret.appendString("await (async () => {\n");
+
+    for (const {when, then} of whenThen) {
+      let condition = "";
+      if (when.get() instanceof Expressions.Cond) {
+        condition = new CondTranspiler().transpile(when, traversal).getCode();
+      } else {
+        throw new Error("CondBodyTranspiler, Expected Cond, todo, " + when.get().constructor.name);
+      }
+
+      let value = "";
+      if (then.get() instanceof Expressions.Source) {
+        value = new SourceTranspiler().transpile(then, traversal).getCode();
+      } else {
+        throw new Error("CondBodyTranspiler, Expected Source, todo, " + then.get().constructor.name);
+      }
+
+      ret.appendString(`if (${condition}) { return ${value}; }\n`);
+    }
+
+    const els = body.findExpressionAfterToken("ELSE");
+    if (els) {
+      if (!(els.get() instanceof Expressions.Source)) {
+        throw new Error("CondBodyTranspiler, Expected Source, todo, " + els.get().constructor.name);
+      }
+      const value = new SourceTranspiler().transpile(els, traversal).getCode();
+      ret.appendString(`return ${value};\n`);
+    } else {
+      ret.appendString(`return ${target};\n`);
+    }
+
+    ret.appendString("})()))");
+    return ret;
+  }
+
+}
---
+++ test/expressions/cond.ts
@@ -0,0 +1,65 @@
+import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string, skipVersionCheck = false) {
+  return runFiles(abap, [{filename: "zfoobar_cond.prog.abap", contents}], {skipVersionCheck});
+}
+
+describe("Running expressions - COND", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it("basic", async () => {
+    const code = `
+FORM foo.
+  DATA(url) = COND string( WHEN 1 = 2 THEN 'foo'
+                           WHEN 1 = 1 THEN 'bar' ).
+  WRITE / url.
+ENDFORM.
+
+START-OF-SELECTION.
+  PERFORM foo.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("bar");
+  });
+
+  it("else", async () => {
+    const code = `
+FORM foo.
+  DATA(url) = COND string( WHEN 1 = 2 THEN 'foo'
+                           ELSE 'bar' ).
+  WRITE / url.
+ENDFORM.
+
+START-OF-SELECTION.
+  PERFORM foo.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("bar");
+  });
+
+  it("no true condition", async () => {
+    const code = `
+FORM foo.
+  DATA(url) = COND string( WHEN 1 = 2 THEN 'foo'
+                           WHEN 1 = 2 THEN 'bar' ).
+  WRITE / url.
+ENDFORM.
+
+START-OF-SELECTION.
+  PERFORM foo.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("");
+  });
+
+});
