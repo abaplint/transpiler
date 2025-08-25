3d98da8bd054b6522aa843542c55da6e2867e07c
Lars Hvam <larshp@hotmail.com>
2025-08-25 14:09:54 +0200

    ALPHA

--- test/expressions/string_template.ts
+++ test/expressions/string_template.ts
@@ -4,8 +4,8 @@ import {AsyncFunction, runFiles} from "../_utils";
 
 let abap: ABAP;
 
-async function run(contents: string) {
-  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+async function run(contents: string, skipVersionCheck = false) {
+  return runFiles(abap, [{filename: "zfoobar_string_templates.prog.abap", contents}], {skipVersionCheck});
 }
 
 describe("Running expressions - String templates", () => {
@@ -337,6 +337,18 @@ DATA lv_value TYPE f.
 lv_value = 5.
 WRITE |{ lv_value STYLE = SCIENTIFIC }|.`;
     const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("5E+00");
+  });
+
+  it.only("ALPHA formatting", async () => {
+    const code = `
+DATA lv_value TYPE n LENGTH 5.
+lv_value = 5.
+WRITE |{ lv_value ALPHA = OUT }|.
+WRITE |{ lv_value ALPHA = IN }|.`;
+    const js = await run(code, true);
     const f = new AsyncFunction("abap", js);
     await f(abap);
     expect(abap.console.get()).to.equal("5E+00");
