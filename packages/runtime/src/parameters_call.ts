1cc2579380fcad1a252f00b0520ed02c24bfaae0
Lars Hvam <larshp@hotmail.com>
2025-08-28 07:58:26 +0200

    refactor

--- packages/runtime/src/parameters_call.ts
+++ packages/runtime/src/parameters_call.ts
@@ -1,8 +1,17 @@
 import {HashedTable, Table} from "./types";
 
 export async function parametersCall(method: (params: any) => Promise<any>, parameters: Table | HashedTable): Promise<void> {
-  const input = parameters.array().reduce((a, v) => ({...a, [v.get().name.get(
-          ).toLowerCase().trimEnd()]: v.get().value.dereference()}), {})
+  const input: Record<string, any> = {};
+
+  for (const v of parameters.array()) {
+    const name = v.get().name.get().toLowerCase().trimEnd();
+    const kind = v.get().kind.get();
+    if (kind === "") {
+      throw new Error("open-abap currently requires KIND to be set in PARAMETER-TABLE calls");
+    }
+    const value = v.get().value.dereference();
+    input[name] = value;
+  }
 
   await method(input);
 }
