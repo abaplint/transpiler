d23ba499ea5e6fa01349838990d9370354a12af7
Lars Hvam <5888506+larshp@users.noreply.github.com>
2025-09-05 13:18:51 +0200

    2.11.70: bugfix SUPPLIED, bugfix CORRESPONDING MAPPING (#1623)

--- packages/transpiler/src/expressions/corresponding_body.ts
+++ packages/transpiler/src/expressions/corresponding_body.ts
@@ -3,6 +3,8 @@ import {Traversal} from "../traversal";
 import {Chunk} from "../chunk";
 import {TypeNameOrInfer} from "./type_name_or_infer";
 import {TranspileTypes} from "../transpile_types";
+import {ComponentChainTranspiler} from "./component_chain";
+import {UniqueIdentifier} from "../unique_identifier";
 
 export class CorrespondingBodyTranspiler {
 
@@ -15,6 +17,13 @@ export class CorrespondingBodyTranspiler {
     let target = TranspileTypes.toType(type);
     let source: Chunk | undefined;
 
+    type mappingType = {
+      componentName: Nodes.ExpressionNode | undefined,
+      componentChain: Nodes.ExpressionNode | undefined,
+    };
+    const mapping: mappingType[] = [];
+    const mappingRow: mappingType = {componentName: undefined, componentChain: undefined};
+
     for (const child of body.getChildren()) {
       const c = child.get();
       if (c instanceof Expressions.Source && child instanceof Nodes.ExpressionNode) {
@@ -22,13 +31,35 @@ export class CorrespondingBodyTranspiler {
       } else if (c instanceof Expressions.CorrespondingBodyBase && child instanceof Nodes.ExpressionNode) {
         source = traversal.traverse(child?.findDirectExpression(Expressions.Source));
         target = `abap.statements.moveCorresponding(${source!.getCode()}, ${target})`;
+      } else if (c instanceof Expressions.CorrespondingBodyMapping && child instanceof Nodes.ExpressionNode) {
+        for (const cc of child.getChildren()) {
+          if (cc.get() instanceof Expressions.ComponentName) {
+            mappingRow.componentName = cc as Nodes.ExpressionNode;
+          } else if (cc.get() instanceof Expressions.ComponentChain) {
+            mappingRow.componentChain = cc as Nodes.ExpressionNode;
+            mapping.push(mappingRow);
+          }
+        }
       } else {
-        throw new Error("CorrespondingBodyTranspiler, todo, " + c.constructor.name);
+        throw new Error("CorrespondingBodyTranspiler, todo, " + c.constructor.name + ", " + body.concatTokens());
       }
     }
 
     const ret = new Chunk();
-    ret.appendString(`abap.statements.moveCorresponding(${source!.getCode()}, ${target})`);
+    const id = UniqueIdentifier.get();
+    ret.appendString("(await (async () => {\n");
+    ret.appendString(`const ${id} = ${target};\n`);
+    ret.appendString(`abap.statements.moveCorresponding(${source!.getCode()}, ${id});\n`);
+
+    for (const map of mapping) {
+      const componentName = map.componentName!.concatTokens().toLowerCase();
+      const chain = new ComponentChainTranspiler().transpile(map.componentChain!, traversal).getCode();
+      ret.appendString(`${id}.get().${componentName}.set(${source!.getCode()}.get().${chain});\n`);
+    }
+
+    ret.appendString("return " + id + ";\n");
+    ret.appendString("})())");
+
     return ret;
   }
 
