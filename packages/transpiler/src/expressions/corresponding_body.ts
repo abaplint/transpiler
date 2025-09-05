e0046085db88aebd17148996b277bf7eb7b8eadb
Lars Hvam <larshp@hotmail.com>
2025-09-05 12:41:46 +0200

    update

--- packages/transpiler/src/expressions/corresponding_body.ts
+++ packages/transpiler/src/expressions/corresponding_body.ts
@@ -15,6 +15,13 @@ export class CorrespondingBodyTranspiler {
     let target = TranspileTypes.toType(type);
     let source: Chunk | undefined;
 
+    type mappingType = {
+      componentName: Nodes.ExpressionNode | undefined,
+      componentChain: Nodes.ExpressionNode | undefined,
+    };
+    const mapping: mappingType[] = [];
+    const mappingRow: mappingType = {componentName: undefined, componentChain: undefined};
+
     for (const child of body.getChildren()) {
       const c = child.get();
       if (c instanceof Expressions.Source && child instanceof Nodes.ExpressionNode) {
@@ -22,6 +29,15 @@ export class CorrespondingBodyTranspiler {
       } else if (c instanceof Expressions.CorrespondingBodyBase && child instanceof Nodes.ExpressionNode) {
         source = traversal.traverse(child?.findDirectExpression(Expressions.Source));
         target = `abap.statements.moveCorresponding(${source!.getCode()}, ${target})`;
+      } else if (c instanceof Expressions.CorrespondingBodyMapping && child instanceof Nodes.ExpressionNode) {
+        for (const cc of child.getChildren()) {
+          if (cc.get() instanceof Expressions.ComponentName) {
+            mappingRow.componentName = cc as Nodes.ExpressionNode;
+          } else if (cc.get() instanceof Expressions.ComponentChain) {
+            mappingRow.componentChain = cc as Nodes.ExpressionNode;
+            mapping.push(mappingRow);
+          }
+        }
       } else {
         throw new Error("CorrespondingBodyTranspiler, todo, " + c.constructor.name + ", " + body.concatTokens());
       }
