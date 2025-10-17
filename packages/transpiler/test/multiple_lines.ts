6de071957eb43db625aee43480c29dc2a530aef2
Lars Hvam <larshp@hotmail.com>
2025-10-17 15:52:48 +0200

    update

--- packages/transpiler/test/multiple_lines.ts
+++ packages/transpiler/test/multiple_lines.ts
@@ -599,7 +599,7 @@ TRY.
     WRITE / 'bar'.
 ENDTRY.`;
 
-    const expected = `let cx = (() => { throw new Error("Void type: cx_root") })();
+    const expected = `let cx = (() => { throw new Error("Void type: CX_ROOT") })();
 try {
   try {
     const unique1 = await (new abap.Classes['CX_ABAP_MESSAGE_DIGEST']()).constructor_();
--- packages/transpiler/test/type_runtime_error.ts
+++ packages/transpiler/test/type_runtime_error.ts
@@ -17,7 +17,7 @@ describe("Unknown types, errors at runtime", () => {
   it("test 2, error", async () => {
     const abap = `DATA foo TYPE REF TO zcl_bar.`;
 
-    const expected = `let foo = (() => { throw new Error("Void type: zcl_bar") })();`;
+    const expected = `let foo = (() => { throw new Error("Void type: ZCL_BAR") })();`;
 
     expect(await runSingle(abap, options)).to.equal(expected);
   });
@@ -36,7 +36,7 @@ foo.set(await (new abap.Classes['ZCL_ABAPGIT_GUI']()).constructor_());`;
     const abap = `DATA foo TYPE REF TO something.
     CREATE OBJECT foo.`;
 
-    const expected = `let foo = (() => { throw new Error("Void type: something") })();
+    const expected = `let foo = (() => { throw new Error("Void type: SOMETHING") })();
 foo.set(await (new abap.Classes['RUNTIME_ERROR']()).constructor_());`;
 
     expect(await runSingle(abap, options)).to.equal(expected);
