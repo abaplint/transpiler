79b955c455432a5d8b9de57c7fcfd3ffea52beb7
Lars Hvam <larshp@hotmail.com>
2026-03-24 09:00:34 +0100

    add testcase

---
+++ test/types/enum.ts
@@ -0,0 +1,33 @@
+import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src/";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running Examples - ENUMs", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it.only("basics", async () => {
+    const code = `
+TYPES: BEGIN OF ENUM ty_cache_policy STRUCTURE cache_policies,
+         use_all,
+         use_none,
+       END OF ENUM ty_cache_policy STRUCTURE cache_policies.
+
+DATA foo TYPE ty_cache_policy.
+WRITE / cache_policies-use_all.
+WRITE / foo.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("USE_ALL\NUSE_ALL");
+  });
+
+});
