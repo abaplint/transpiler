ece139fd1285ded447675516f6658742eb8e504f
Lars Hvam <larshp@hotmail.com>
2026-05-03 11:50:47 +0200

    add testcase

---
+++ test/statements/return.ts
@@ -0,0 +1,39 @@
+import {expect} from "chai";
+import {ABAP, MemoryConsole} from "../../packages/runtime/src";
+import {AsyncFunction, runFiles} from "../_utils";
+
+let abap: ABAP;
+
+async function run(contents: string) {
+  return runFiles(abap, [{filename: "zfoobar.prog.abap", contents}]);
+}
+
+describe("Running statements - RETURN", () => {
+
+  beforeEach(async () => {
+    abap = new ABAP({console: new MemoryConsole()});
+  });
+
+  it.only("basic", async () => {
+    const code = `
+CLASS LCL DEFINITION.
+  PUBLIC SECTION.
+    METHODS MAIN RETURNING VALUE(RESULT) TYPE I.
+ENDCLASS.
+
+CLASS LCL IMPLEMENTATION.
+  METHOD MAIN.
+    return 2 + 2.
+  ENDMETHOD.
+ENDCLASS.
+
+start-of-selection.
+  DATA(RESULT) = LCL=>MAIN( ).
+  WRITE / RESULT.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+    expect(abap.console.get()).to.equal("4");
+  });
+
+});
