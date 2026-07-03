e6a41606492b90f06c73bccc4db12327ba2a637e
Lars Hvam <larshp@hotmail.com>
2026-07-03 20:11:41 +0200

    add basics

--- packages/extras/src/handlers/handle_ddls.ts
+++ packages/extras/src/handlers/handle_ddls.ts
@@ -1,5 +1,5 @@
 import * as abaplint from "@abaplint/core";
-import {IOutputFile, ITranspilerOptions, ITranspilerPlugin} from "@abaplint/transpiler";
+import {DatabaseSetupResult, IOutputFile, ITranspilerOptions, ITranspilerPlugin} from "@abaplint/transpiler";
 
 export class HandleDDLS implements ITranspilerPlugin {
 
@@ -14,6 +14,46 @@ export class HandleDDLS implements ITranspilerPlugin {
 
     // no runtime relevance, accept the object but produce no output
     return [];
+  }
+
+  public amendDatabaseSetup(dbSetup: DatabaseSetupResult, reg: abaplint.IRegistry, _options: ITranspilerOptions): void {
+    for (const obj of reg.getObjects()) {
+      if (!(obj instanceof abaplint.Objects.DataDefinition)) {
+        continue;
+      }
+
+      const view = this.buildView(obj, reg, "'");
+      if (view !== undefined) {
+        dbSetup.schemas.sqlite.push(view);
+      }
+
+      const quotedView = this.buildView(obj, reg, "\"");
+      if (quotedView !== undefined) {
+        dbSetup.schemas.pg.push(quotedView);
+        dbSetup.schemas.snowflake.push(quotedView);
+      }
+    }
+  }
+
+  private buildView(obj: abaplint.Objects.DataDefinition, reg: abaplint.IRegistry, quote: string): string | undefined {
+    obj.parse();
+    const parsed = obj.getParsedData();
+    const source = parsed?.sources[0];
+    if (parsed === undefined || source === undefined || parsed.fields.length === 0) {
+      return undefined;
+    }
+
+    const table = reg.getObject("TABL", source.name);
+    if (!(table instanceof abaplint.Objects.Table)) {
+      return undefined;
+    }
+
+    const q = (name: string) => quote + name.toLowerCase() + quote;
+    const viewName = parsed.definitionName || obj.getName();
+    const columns = parsed.fields.map(field =>
+      q(source.name) + "." + q(field.name) + " AS " + field.name.toLowerCase()).join(", ");
+
+    return `CREATE VIEW ${q(viewName)} AS SELECT ${columns} FROM ${q(source.name)};`;
   }
 
 }
