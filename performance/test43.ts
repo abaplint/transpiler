d7060e1846b2014aefd505df6f46cf4fc846c921
Lars Hvam <5888506+larshp@users.noreply.github.com>
2026-07-30 13:39:07 +0200

    Increase loop iteration from 1000 to 2000

--- performance/test43.ts
+++ performance/test43.ts
@@ -2,7 +2,7 @@ export const test43 = `
 DATA lt_tab TYPE STANDARD TABLE OF string WITH DEFAULT KEY.
 DATA lv_str TYPE string.
 
-DO 1000 TIMES.
+DO 2000 TIMES.
   APPEND 'foobar' TO lt_tab.
 ENDDO.
 
