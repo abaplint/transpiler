37bdd693d45c8fe70e15ed7f7be9956f88d48118
Lars HVam <larshp@hotmail.com>
2026-08-09 18:31:54 +0200

    add testcase

--- test/statements/assign.ts
+++ test/statements/assign.ts
@@ -941,6 +941,26 @@ ASSERT <value> IS NOT ASSIGNED.`;
     await f(abap);
   });
 
+  it("ASSIGN, data reference namespaced component", async () => {
+    const code = `
+TYPES:
+  BEGIN OF ty_data,
+    /foo/test TYPE string,
+  END OF ty_data.
+DATA structure      TYPE ty_data.
+DATA data_reference TYPE REF TO data.
+DATA component_name TYPE string VALUE '/FOO/TEST'.
+FIELD-SYMBOLS <value> TYPE any.
+structure-/foo/test = 'TEST'.
+GET REFERENCE OF structure INTO data_reference.
+ASSIGN data_reference->(component_name) TO <value>.
+ASSERT sy-subrc = 0.
+ASSERT <value> = 'TEST'.`;
+    const js = await run(code);
+    const f = new AsyncFunction("abap", js);
+    await f(abap);
+  });
+
   it("ASSIGN, table expression should not throw", async () => {
     const code = `
 TYPES: BEGIN OF ty,
